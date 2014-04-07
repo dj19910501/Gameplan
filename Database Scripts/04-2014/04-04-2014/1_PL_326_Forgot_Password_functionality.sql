6092471dd0025be0be094289eac5144f070a5b95
unknown <dharmrajm@.indusa.com>
2014-04-07 13:05:21 +0530

    Modified DB script for Ticket #326.

--- Database Scripts/04-2014/04-04-2014/1_PL_326_Forgot_Password_functionality.sql
+++ Database Scripts/04-2014/04-04-2014/1_PL_326_Forgot_Password_functionality.sql
@@ -74,3 +74,11 @@ IF NOT EXISTS (SELECT * FROM sys.objects o WHERE o.object_id = object_id(N'[dbo]
 BEGIN
 	  ALTER TABLE [User] ADD CONSTRAINT FK_User_SecurityQuestion FOREIGN KEY (SecurityQuestionId) REFERENCES SecurityQuestion(SecurityQuestionId)
 END
+
+Go
+IF NOT EXISTS(SELECT TOP 1 * FROM Notification WHERE [NotificationInternalUseOnly] = 'ResetPasswordLink')
+BEGIN
+
+INSERT [dbo].[Notification] ([NotificationId], [NotificationInternalUseOnly], [Title], [Description], [NotificationType], [EmailContent], [IsDeleted], [CreatedDate], [CreatedBy], [ModifiedDate], [ModifiedBy], [Subject]) VALUES (92, N'ResetPasswordLink', N'ResetPasswordLink', N'When user requested to reset password', N'CM', N' This e-mail has been sent in response to your request for help resetting your Gameplan password.To initiate the process for resetting the password for your Gameplan account, follow the link below: <br />[PasswordResetLinkToBeReplaced]<br/>Note that this e-mail will expire on [ExpireDateToBeReplaced]. If it expires before you are able to complete the password reset process, you may request a new password reset.<br/>Thank you,<br/>Gameplan Administrator', 0, CAST(0x0000A2FC00C8772A AS DateTime), N'092f54df-4c71-4f2f-9d21-0ae16155e5c1', NULL, NULL, N'Gameplan : Password reset link')
+
+END
