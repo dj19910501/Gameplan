d28acc69f18ee485190fb0edd33ff94658e8401d
unknown <bhaveshd@.indusa.com>
2014-04-22 15:51:59 +0530

    Changes related to #348.

--- RevenuePlanner/Controllers/ReportController.cs
+++ RevenuePlanner/Controllers/ReportController.cs
@@ -2746,7 +2746,7 @@ namespace RevenuePlanner.Controllers
             double projectedRevenueValue = GetProjectedRevenueData(tacticIds).AsEnumerable().AsQueryable().Where(mr => includeMonthUpCurrent.Contains(mr.Field<string>(ColumnMonth))).Sum(mr => mr.Field<double>(ColumnValue));
             if (projectedRevenueValue != 0 && actualRevenueValue >= projectedRevenueValue)
             {
-                percentageValue = ((actualRevenueValue - projectedRevenueValue) / projectedRevenueValue) * 100;
+                percentageValue = Math.Round(((actualRevenueValue - projectedRevenueValue) / projectedRevenueValue) * 100, 2);
             }
             return percentageValue;
         }
