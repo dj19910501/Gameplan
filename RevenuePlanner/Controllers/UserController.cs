dfc4c76d626a703fe0b0a15929d1273ae0e94c93
unknown <udayab@.indusa.com>
2014-07-07 18:30:20 +0530

    PL Ticket #555

--- RevenuePlanner/Controllers/UserController.cs
+++ RevenuePlanner/Controllers/UserController.cs
@@ -972,12 +972,12 @@ namespace RevenuePlanner.Controllers
         /// <param name="file">user photo</param>
         /// <returns></returns>
         [HttpPost]
-        public ActionResult Edit(UserModel form, HttpPostedFileBase file)
+        public ActionResult Edit(UserModel form, HttpPostedFileBase file, FormCollection formcollection)//formcollection added by uday#555
         {
             // Added by Sohel Pathan on 19/06/2014 for PL ticket #537 to implement user permission Logic
             ViewBag.IsIntegrationCredentialCreateEditAuthorized = AuthorizeUserAttribute.IsAuthorized(Enums.ApplicationActivity.IntegrationCredentialCreateEdit);
             ViewBag.IsUserAdminAuthorized = AuthorizeUserAttribute.IsAuthorized(Enums.ApplicationActivity.UserAdmin);
-
+            var flag = formcollection["removeflag"];//added by uday #555
             // Added by Sohel Pathan on 26/06/2014 for PL ticket #517
             ViewBag.isForDelete = "false";
 
@@ -1033,10 +1033,22 @@ namespace RevenuePlanner.Controllers
                             objUser.ProfilePhoto = data;
                         }
                     }
-                    else
+                    else if (flag != null && flag != "false" && flag != "" && flag != string.Empty)//added by uday #555
                     {
                         objUser.ProfilePhoto = null;
                     }
+                    else
+                    {
+                        //add uday
+                        BDSService.User objUsernew = objBDSServiceClient.GetTeamMemberDetails(form.UserId, Sessions.ApplicationId);
+                        if (objUsernew != null)
+                        {
+                            // objUser.ProfilePhoto = form.ProfilePhoto;
+                            objUser.ProfilePhoto = objUsernew.ProfilePhoto;
+                        }
+                        //end add uday
+                        //objUser.ProfilePhoto = null;
+                    }//#555
                     objUser.ClientId = form.ClientId;
                     objUser.BusinessUnitId = form.BusinessUnitId;
                     objUser.GeographyId = form.GeographyId;
