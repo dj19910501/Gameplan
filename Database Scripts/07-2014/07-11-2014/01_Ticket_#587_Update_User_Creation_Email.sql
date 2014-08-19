62d8932e00067841c945f8f4484ac48602a77a80
maninder.singh@indusa.com <maninder.singh@indusa.com>
2014-08-19 20:18:48 +0530

    Removed 07-2014 folder.

--- Database Scripts/07-2014/07-11-2014/01_Ticket_#587_Update_User_Creation_Email.sql
+++
@@ -1,9 +0,0 @@
-
---Execute this script on MRP Database--
-
-IF EXISTS(SELECT 1 FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_SCHEMA='dbo' AND TABLE_NAME='Notification' AND COLUMN_NAME = 'EmailContent')
-BEGIN
-
-UPDATE Notification SET EmailContent='Dear [NameToBeReplaced],<br><br>An account has been created for you in Bulldog Gameplan. Here are your credentials:<br><br><table><tr><td>Username</td><td>:</td><td>[LoginToBeReplaced]</td></tr><tr><td>Password</td><td>:</td><td>[PasswordToBeReplaced]</td></tr></table><br>Click on the link and start planning! <a href=[ApplicationLink]>Log into Bulldog Gameplan</a><br><br>Thank You,<br>Bulldog Gameplan Admin' WHERE NotificationInternalUseOnly='UserCreated'
-
-END
