79b5dbc9499d3b99d7b83a2f488ff40b82b605d3
unknown <kalpeshs@.indusa.com>
2014-08-25 15:35:01 +0530

    #697 Changes in Script : Kalpesh Sharma

--- Database Scripts/08-2014/08-25-2014/01_PL_697_Add_None_LineItemType_Model.sql
+++ Database Scripts/08-2014/08-25-2014/01_PL_697_Add_None_LineItemType_Model.sql
@@ -4,21 +4,14 @@
 
 INSERT INTO LineItemType
 SELECT ModelId
-	,'None' --Model Title
-	,'None' -- Description
-	,0  -- Is Deleted
-	,(Select GETDATE()) -- Created Date 
-	,'14D7D588-CF4D-46BE-B4ED-A74063B67D66' --Created By
-	,NULL -- Modified Date
-	,NULL -- Modified By
+       ,'None' --Model Title
+       ,'None' -- Description
+       ,0  -- Is Deleted
+       ,(Select GETDATE()) -- Created Date 
+       ,'14D7D588-CF4D-46BE-B4ED-A74063B67D66' --Created By
+       ,NULL -- Modified Date
+       ,NULL -- Modified By
 FROM Model
-WHERE ModelId NOT IN (
-		SELECT DISTINCT LIT.ModelId
-		FROM LineItemType LIT
-		WHERE LIT.ModelId IN (
-				SELECT ModelId
-				FROM [dbo].[LineItemType]
-				WHERE Title = 'None'
-				)
-		)
- Go
+WHERE ModelId NOT IN ( SELECT distinct ModelId FROM [dbo].[LineItemType] WHERE Title = 'None')
+Go
+
