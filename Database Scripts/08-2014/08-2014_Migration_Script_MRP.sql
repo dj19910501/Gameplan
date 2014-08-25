d917a6ccfb2596c1b0759ae1cf23733a7a98fc3d
maninder.singh@indusa.com <maninder.singh@indusa.com>
2014-08-25 18:46:19 +0530

    Added insert script for existing models.

--- Database Scripts/08-2014/08-2014_Migration_Script_MRP.sql
+++ Database Scripts/08-2014/08-2014_Migration_Script_MRP.sql
@@ -1115,7 +1115,21 @@ EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'FK - Refers to
 END
 GO 
 
+--------------01_PL_697_Add_None_LineItemType_Model.sql
+-- Created By : Kalpesh Sharma
+-- 08/25/2014
+-- Identify that Model has None LineItemType or not , if model has no None LineItemType at that time we have to insert into LineItemType table  
 
-
-
+INSERT INTO LineItemType
+SELECT ModelId
+       ,'None' --Model Title
+       ,'None' -- Description
+       ,0  -- Is Deleted
+       ,(Select GETDATE()) -- Created Date 
+       ,'14D7D588-CF4D-46BE-B4ED-A74063B67D66' --Created By
+       ,NULL -- Modified Date
+       ,NULL -- Modified By
+FROM Model
+WHERE ModelId NOT IN ( SELECT distinct ModelId FROM [dbo].[LineItemType] WHERE Title = 'None')
+Go
 
