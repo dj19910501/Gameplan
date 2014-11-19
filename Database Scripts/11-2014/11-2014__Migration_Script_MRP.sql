f2e27092eda0bdc198965d7c7b73850f58fa2200
maninder.singh@indusa.com <maninder.singh@indusa.com>
2014-11-19 13:13:22 +0530

    Merged scripts created on 18th with migration script

--- Database Scripts/11-2014/11-2014__Migration_Script_MRP.sql
+++ Database Scripts/11-2014/11-2014__Migration_Script_MRP.sql
@@ -47,3 +47,22 @@ BEGIN
 	END
 END
 
+GO
+
+----01_PL_955_UI_hangs_on_clicking_Suggested_improvements.sql
+BEGIN
+	--Update Program
+	Update	Plan_Campaign_Program SET IsDeleted = 1
+	WHERE PlanCampaignId IN
+	(SELECT PlanCampaignId FROM Plan_Campaign WHERE IsDeleted = 1)
+
+	-- Update Tactic
+	Update	Plan_Campaign_Program_Tactic SET IsDeleted = 1
+	WHERE PlanProgramId IN 
+	(SELECT PlanProgramId FROM Plan_Campaign_Program WHERE IsDeleted = 1)
+
+	-- Update Line Item
+	Update	Plan_Campaign_Program_Tactic_LineItem SET IsDeleted = 1
+	WHERE PlanTacticId IN 
+	(SELECT PlanTacticId FROM Plan_Campaign_Program_Tactic WHERE IsDeleted = 1)
+END
