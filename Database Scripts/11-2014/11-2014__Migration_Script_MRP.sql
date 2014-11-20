109648bea8ac7695fa8bab6679eec65d1013e28e
maninder.singh@indusa.com <maninder.singh@indusa.com>
2014-11-20 19:26:42 +0530

    Merged script created on 19th with migration scripts.

--- Database Scripts/11-2014/11-2014__Migration_Script_MRP.sql
+++ Database Scripts/11-2014/11-2014__Migration_Script_MRP.sql
@@ -66,3 +66,28 @@ BEGIN
 	WHERE PlanTacticId IN 
 	(SELECT PlanTacticId FROM Plan_Campaign_Program_Tactic WHERE IsDeleted = 1)
 END
+
+GO
+
+BEGIN
+	-- ======================================================================================
+	-- Created By : Sohel Pathan
+	-- Created Date : 14/11/2014
+	-- Description : Add email entry for TacticOwnerChanged in Notification table.
+	-- ======================================================================================
+
+	IF EXISTS(SELECT 1 FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA='dbo' AND TABLE_NAME='Notification')
+	BEGIN
+
+		IF EXISTS (SELECT 1 FROM [Notification] WHERE NotificationInternalUseOnly = 'TacticOwnerChanged')
+		BEGIN
+
+			UPDATE [NOTIFICATION] SET EmailContent = REPLACE(EmailContent, '<br><br>Thank You,<br>Bulldog Gameplan Admin', '<br>Thank You,<br>Bulldog Gameplan Admin')
+			WHERE NotificationInternalUseOnly = 'TacticOwnerChanged'
+
+		END
+
+	END
+END
+
+GO
