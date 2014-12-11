30dfcd13644a963bf709d85e3612f1454f56b04f
unknown <manojl@.indusa.com>
2014-12-11 17:52:32 +0530

    DB script added

---
+++ Database Scripts/12-2014/12-2014_Migration_Script_BDSAuth.sql
@@ -0,0 +1,9 @@
+ ---Execute this script on BDSAuth database.
+----Updated Release month.
+BEGIN
+	IF EXISTS(Select 1 from [dbo].[Application] Where [Code] = 'MRP')
+	BEGIN
+		UPDATE [dbo].[Application] set ReleaseVersion= '2014.December' Where [Code] = 'MRP'
+	END
+END
+GO
