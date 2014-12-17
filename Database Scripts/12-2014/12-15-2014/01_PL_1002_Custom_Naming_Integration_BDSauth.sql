46bb99e1d7996fd4f5d98db9f5a6f799f8361204
mitesh.vaishanav@indusa.com <mitesh.vaishanav@indusa.com>
2014-12-17 12:54:33 +0530

    I added db script

--- Database Scripts/12-2014/12-15-2014/01_PL_1002_Custom_Naming_Integration_BDSauth.sql
+++ Database Scripts/12-2014/12-15-2014/01_PL_1002_Custom_Naming_Integration_BDSauth.sql
@@ -13,6 +13,14 @@ IF NOT EXISTS(SELECT * FROM sys.columns WHERE [name] = N'ActivityType' AND [obje
 Go
 IF EXISTS(SELECT * FROM sys.columns WHERE [name] = N'ActivityType' AND [object_id] = OBJECT_ID(N'Application_Activity'))
 	    BEGIN
-		UPDATE dbo.Application_Activity SET ActivityType='User'
+		UPDATE dbo.Application_Activity SET ActivityType='User' WHERE ActivityType IS NULL
+
+		IF NOT EXISTS (SELECT * FROM dbo.Application_Activity WHERE Code='CustomCampaignNameConvention')
+		BEGIN
+		INSERT INTO [dbo].[Application_Activity]
+           ([ApplicationActivityId],[ApplicationId],[ActivityTitle],[Code],[CreatedDate],[ActivityType])
+        VALUES
+           ('22','1c10d4b9-7931-4a7c-99e9-a158ce158951','Generate custom campaign name','CustomCampaignNameConvention',GETDATE(),'Client')
+		   END
 		END
 
