a388e8aa991ce40b483ce40d7594fc697cb2fb66
unknown <manojl@.indusa.com>
2014-12-17 20:23:22 +0530

    Merged script for 17th sprint

---
+++ Database Scripts/12-2014/12-2014_Migration_Script_BDSAuth_17th.sql
@@ -0,0 +1,47 @@
+-- ======================================================================================
+-- Created By : Mitesh Vaishnav
+-- Created Date : 12/12/2014
+-- Description : Custom Naming: Integration
+-- ======================================================================================
+
+
+
+IF NOT EXISTS(SELECT * FROM sys.columns WHERE [name] = N'ActivityType' AND [object_id] = OBJECT_ID(N'Application_Activity'))
+	    BEGIN
+		    ALTER TABLE [Application_Activity] ADD ActivityType NVARCHAR(255) NULL
+	    END
+Go
+IF EXISTS(SELECT * FROM sys.columns WHERE [name] = N'ActivityType' AND [object_id] = OBJECT_ID(N'Application_Activity'))
+	    BEGIN
+		UPDATE dbo.Application_Activity SET ActivityType='User' WHERE ActivityType IS NULL
+
+		IF NOT EXISTS (SELECT * FROM dbo.Application_Activity WHERE Code='CustomCampaignNameConvention')
+		BEGIN
+		IF EXISTS (SELECT * FROM dbo.Application WHERE IsDeleted=0 AND Code='MRP')
+		BEGIN
+        DECLARE @ApplicationId UNIQUEIDENTIFIER
+		DECLARE @count INT 
+		SELECT TOP(1) @ApplicationId=ApplicationId FROM dbo.Application WHERE IsDeleted=0 AND Code='MRP'
+		SELECT @count=MAX(ApplicationActivityId)  FROM dbo.Application_Activity
+		SELECT @count=@count+1
+		INSERT INTO [dbo].[Application_Activity]
+           ([ApplicationActivityId],[ApplicationId],[ActivityTitle],[Code],[CreatedDate],[ActivityType])
+        VALUES
+           (@count,@ApplicationId,'Generate custom campaign name','CustomCampaignNameConvention',GETDATE(),'Client')
+		   END
+
+		   END
+		END
+
+GO
+
+-- ======================================================================================
+-- Created By : Mitesh Vaishnav
+-- Created Date : 17/12/2014
+-- Description : Custom Naming: Integration
+-- ======================================================================================
+
+IF EXISTS(SELECT 1 FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA='dbo' AND TABLE_NAME='Client_Activity')
+BEGIN
+DROP TABLE Client_Activity
+END
---
+++ Database Scripts/12-2014/12-2014__Migration_Script_MRP_17th.sql
@@ -0,0 +1,39 @@
+-- ======================================================================================
+-- Created By : Mitesh Vaishnav
+-- Created Date : 12/12/2014
+-- Description : Custom Naming: Integration
+-- ======================================================================================
+
+IF NOT EXISTS(SELECT 1 FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA='dbo' AND TABLE_NAME='Client_Activity')
+BEGIN
+
+CREATE TABLE [dbo].[Client_Activity](
+	[ClientActivityId] [int] IDENTITY(1,1) NOT NULL,
+	[ClientId] [uniqueidentifier] NOT NULL,
+	[ApplicationActivityId] [int] NOT NULL,
+	[CreatedBy] [uniqueidentifier] NOT NULL,
+	[CreatedDate] [datetime] NOT NULL,
+ CONSTRAINT [PK_Client_Activity] PRIMARY KEY CLUSTERED 
+(
+	[ClientActivityId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'An auto increment primary key.' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'Client_Activity', @level2type=N'COLUMN',@level2name=N'ClientActivityId'
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'FK - Refers to associated ClientId.' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'Client_Activity', @level2type=N'COLUMN',@level2name=N'ClientId'
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'FK - Refers associated application activity.' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'Client_Activity', @level2type=N'COLUMN',@level2name=N'ApplicationActivityId'
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'Refers to associated UserId.' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'Client_Activity', @level2type=N'COLUMN',@level2name=N'CreatedBy'
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'Date on which the record was created.' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'Client_Activity', @level2type=N'COLUMN',@level2name=N'CreatedDate'
+
+END
+
+IF NOT EXISTS(SELECT * FROM sys.columns WHERE [name] = N'CustomNamingPermission' AND [object_id] = OBJECT_ID(N'IntegrationInstance'))
+	    BEGIN
+		    ALTER TABLE [IntegrationInstance] ADD CustomNamingPermission bit DEFAULT (1) NOT NULL
+	    END
+
+GO
