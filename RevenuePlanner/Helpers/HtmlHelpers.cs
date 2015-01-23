ce74c301c014add957b31e71b92e241527aa3fc6
mitesh.vaishanav@indusa.com <mitesh.vaishanav@indusa.com>
2015-01-23 17:12:08 +0530

    I have made changes related to PL ticket #1076 and #1138

--- RevenuePlanner/Helpers/HtmlHelpers.cs
+++ RevenuePlanner/Helpers/HtmlHelpers.cs
@@ -3580,79 +3580,6 @@ namespace RevenuePlanner.Helpers
         #region Custom Fields
 
         /// <summary>
-        /// Added by Mitesh Vaishnav for PL ticket #718
-        /// function generate html output for custom fields of campaign,program or tactic
-        /// </summary>
-        /// <param name="id">Plan Tactic Id or Plan Campaign Id or Plan Program Id</param>
-        /// <param name="section">Parameter contains value from enum EntityType like Campaign or Program or Tactic.</param>
-        /// <returns>If Plan Tactic or Plan Campaign or Plan Program contains custom fields than returns html string else empty string</returns>
-        //public static MvcHtmlString GenerateCustomFields(int id, string section)
-        //{
-        //    //list of custom fields for particular campaign or Program or Tactic
-        //    List<CustomFieldModel> customFieldList = Common.GetCustomFields(id, section);
-        //    string sb = string.Empty;
-        //    if (customFieldList.Count != 0)
-        //    {
-        //        //count variable for defining alternate raw
-        //        int count = 0;
-        //        foreach (var item in customFieldList)
-        //        {
-        //            string classname = "content-row";
-        //            if (count % 2 != 0)
-        //            {
-        //                classname += " alternate";
-        //            }
-        //            //check if custom field type is textbox then generate textbox and if custom field type is dropdownlist then generate dropdownlist
-        //            if (item.customFieldType == Enums.CustomFieldType.TextBox.ToString())
-        //            {
-        //                sb += "<div class=\""+classname+" \"><label class=\"padding-left4\" title=\"" + item.name + "\">" + Common.TruncateLable(item.name, 33) + "</label>";
-        //                //When item value contains double quots then it would be replaced 
-        //                string customFieldEntityValue = item.value != null ? item.value.Replace("\"", "&quot;") : string.Empty;
-        //                sb += "<input id=\"cf_" + item.customFieldId + "\" type=\"text\" value=\"" + customFieldEntityValue + "\" cf_id=\"" + item.customFieldId + "\" maxlength=\"255\"";
-        //                //If custom field is required than add attribute require
-        //                if (item.isRequired)
-        //                {
-        //                    sb += " require=\"true\"";
-        //                }
-        //                sb += "></div>";
-        //            }
-        //            else if (item.customFieldType == Enums.CustomFieldType.DropDownList.ToString())
-        //            {
-        //                classname += " margin-bottom10";
-        //                sb += "<div class=\"" + classname + " \"><label class=\"padding-left4\" title=\"" + item.name + "\">" + Common.TruncateLable(item.name, 33) + "</label>";
-        //                sb += "<span class=\"verticalIdSelectBox\">  <select id=\"cf_" + item.customFieldId + "\" cf_id=\"" + item.customFieldId + "\" class=\"ddlStyle\"";
-        //                //If custom field is required than add attribute require
-        //                if (item.isRequired)
-        //                {
-        //                    sb += " require=\"true\"";
-        //                }
-        //                sb += "><option value=\"\">Please Select</option>";
-        //                //set dropdown option values
-        //                if (item.option.Count != 0)
-        //                {
-        //                    foreach (var objOption in item.option)
-        //                    {
-        //                        //check - if custom field's value inserted before from dropdownlist then set it as selected
-        //                        if (item.value != objOption.customFieldOptionId.ToString())
-        //                        {
-        //                            sb += "<option value=\"" + objOption.customFieldOptionId + "\">" + objOption.value + "</option>";
-        //                        }
-        //                        else
-        //                        {
-        //                            sb += "<option value=\"" + objOption.customFieldOptionId + "\" selected=true>" + objOption.value + "</option>";
-        //                        }
-        //                    }
-        //                }
-        //                sb += "</select></span></div>";
-
-        //            }
-        //            count = count + 1;
-        //        }
-        //    }
-        //    return new MvcHtmlString(sb.ToString());
-        //}
-
-        /// <summary>
         /// Added by Mitesh Vaishnav for PL ticket #844
         /// function generate html output for custom fields of campaign,program or tactic into inspect popup screen
         /// </summary>
@@ -3679,10 +3606,13 @@ namespace RevenuePlanner.Helpers
                     {
                         className += "\" style=\"clear:both;\"";
                     }
+                    if (item.customFieldType == Enums.CustomFieldType.TextBox.ToString() || item.option.Count > 0)
+                    {
                     if (item.isRequired)
                         sb.Append("<div class=\"" + className + "\"><p title=\"" + item.name + "\" class=\"ellipsis-left\">" + item.name + "</p> <span class='required-asterisk'>*</span>");
                     else
                         sb.Append("<div class=\"" + className + "\"><p title=\"" + item.name + "\" class=\"ellipsis\">" + item.name + "</p>");
+                    }
 
                     //check if custom field type is textbox then generate textbox and if custom field type is dropdownlist then generate dropdownlist
                     if (item.customFieldType == Enums.CustomFieldType.TextBox.ToString())
@@ -3767,7 +3697,7 @@ namespace RevenuePlanner.Helpers
                                         name += objOption.value + ",";
                                         enableCheck = "checked=\"checked\"";
                                     }
-                                    sb.Append("<tr><td class=\"first_show\"><label><input cf_id=\"" + item.customFieldId + "\" name=\"" + item.customFieldId + "\" type=\"checkbox\" value=\"" + objOption.customFieldOptionId + "\" class=\"  technology_chkbx\" " + enableCheck + " ><label class=\"lable_inline\"><p class=\"text_ellipsis\" title=\"" + objOption.value + "\">" + objOption.value + "</p></label></label></td></tr>");
+                                    sb.Append("<tr><td class=\"first_show\"><label><input cf_id=\"" + item.customFieldId + "\" name=\"" + item.customFieldId + "\" type=\"checkbox\" value=\"" + objOption.customFieldOptionId + "\" class=\"  technology_chkbx\" " + enableCheck + " ><label class=\"lable_inline\"><p class=\"text_ellipsis\" style=\"width:180px;\" title=\"" + objOption.value + "\">" + objOption.value + "</p></label></label></td></tr>");
                                 }
                                 sb.Append("</table></div></div></div>");
                                 if (name.Length > 0)
