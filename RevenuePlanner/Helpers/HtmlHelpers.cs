94b28ddc865a3c9e9d1fb2e788d40abe447747ca
unknown <viralk@.indusa.com>
2015-02-11 11:57:47 +0530

    I have made required changes to resolve issues for PL ticket #1187.

--- RevenuePlanner/Helpers/HtmlHelpers.cs
+++ RevenuePlanner/Helpers/HtmlHelpers.cs
@@ -3785,7 +3785,7 @@ namespace RevenuePlanner.Helpers
                                         enableCheck = "checked=\"checked\"";
                                         
                                     }
-                                    sb.Append("<tr class=\"" + trhover + "\"><td class=\"first_show\"><label><input cf_id=\"" + item.customFieldId + "\" name=\"" + item.customFieldId + "\" type=\"checkbox\" value=\"" + objOption.customFieldOptionId + "\" class=\"  technology_chkbx\" " + enableCheck + "" + displayCheckbox + "><label class=\"lable_inline\"><p class=\"text_ellipsis " + singlehover + "\" title=\"" + objOption.value + "\">" + objOption.value + "</p></label></label></td></tr>");
+                                    sb.Append("<tr class=\"" + trhover + "\"><td class=\"first_show\"><label class=\"lblCustomCheckbox\"><input cf_id=\"" + item.customFieldId + "\" name=\"" + item.customFieldId + "\" type=\"checkbox\" value=\"" + objOption.customFieldOptionId + "\" class=\"  technology_chkbx\" " + enableCheck + "" + displayCheckbox + "><label class=\"lable_inline\"><p class=\"text_ellipsis " + singlehover + "\" title=\"" + objOption.value + "\">" + objOption.value + "</p></label></label></td></tr>");
                                 }
                                 sb.Append("</tbody><tfoot class=\"programcampaignborder\"><tr><td colspan=\"3\" class=\"advance\"><a href=\"#\" class=\"advance_a\" mode=\"" + selectionMode + "\"><span class=\"swap-text\">" + footerText + "</span></a></td></tr></tfoot></table></div></div></div>");
                                 if (name.Length > 0)
