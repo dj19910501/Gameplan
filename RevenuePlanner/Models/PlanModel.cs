8a9c294ea09ec0a4231562441d1b409eb6046731
unknown <bhaveshd@.indusa.com>
2015-03-04 17:11:41 +0530

    changes related to #711

--- RevenuePlanner/Models/PlanModel.cs
+++ RevenuePlanner/Models/PlanModel.cs
@@ -58,6 +58,8 @@ namespace RevenuePlanner.Models
         public int PlanCampaignId { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
+        public string Owner { get; set; }
+        public Guid OwnerId { get; set; }
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
         //public long? INQs { get; set; }
@@ -82,6 +84,8 @@ namespace RevenuePlanner.Models
         public int PlanCampaignId { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
+        public string Owner { get; set; }
+        public Guid OwnerId { get; set; }
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
         //public long? INQs { get; set; }
