c07a0746d67217502a9212c506cd1f9735341451
unknown <bhaveshd@.indusa.com>
2015-05-22 12:48:23 +0530

    Changes related to Script.

--- Database Scripts/05-2015/05-2015_Migration_Script_MRP.sql
+++ Database Scripts/05-2015/05-2015_Migration_Script_MRP.sql
@@ -8,3 +8,30 @@ GO
 GO
 UPDATE dbo.[Notification] SET EmailContent=REPLACE(EmailContent,'bulldog','Hive9') WHERE EmailContent LIKE '%bulldog%'
 GO
+
+
+Go
+IF EXISTS(SELECT 1 FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA='dbo' AND TABLE_NAME='CustomFieldOption')
+ begin
+ IF NOT EXISTS(SELECT * FROM sys.columns WHERE [name] =  N'IsDeleted'  AND [object_id] = OBJECT_ID(N'CustomFieldOption'))
+ begin
+ ALTER TABLE CustomFieldOption
+ADD IsDeleted bit  NOT NULL 
+CONSTRAINT Delete_Option DEFAULT 0
+end
+end
+GO
+
+GO
+WITH tblTemp as
+(
+SELECT ROW_NUMBER() Over(PARTITION BY ModelId,StageId,StageType ORDER BY StageId)
+   As RowNumber,* FROM Model_Stage
+)
+DELETE From Model_Stage WHERE ModelStageId in 
+(
+SELECT ModelStageId FROM tblTemp
+WHERE RowNumber > 1
+)
+
+GO
