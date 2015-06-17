16f394f2f4f04f810364be46c5e26372b16aaec6
unknown <bhaveshd@.indusa.com>
2015-06-17 18:27:43 +0530

    Changes related to DB script.

--- Database Scripts/05-2015/05-04-2015/BDSAuth_Branding_From_Bulldog_To_Hive9.sql
+++
@@ -1,8 +0,0 @@
--- ======================================================================================
--- Created By : Mitesh Vaishnav
--- Created Date : 05/01/2015
--- Description :replace bulldog with Hive9
--- ======================================================================================
-GO
-UPDATE dbo.[Application] SET Name=REPLACE(Name,'bulldog','Hive9'),Description=REPLACE(Description,'bulldog','Hive9')
-GO
