82ce47a5e8111ee8a3fba3831ecf16b1978173ec
Brad Gray <brad@hive9.com>
2015-07-16 10:20:23 -0500

    quick updates to new WorkFront database script

--- Database Scripts/07-2015/07-16-2015_WorkFrontDatabaseChanges_MRP.sql
+++ Database Scripts/07-2015/07-16-2015_WorkFrontDatabaseChanges_MRP.sql
@@ -6,9 +6,7 @@
 
 --Add WorkFront entry to IntegrationType
 IF NOT EXISTS(SELECT * FROM [dbo].[IntegrationType] WHERE [Title] = 'WorkFront' AND [APIURL] = '.attask-ondemand.com/attask/api')
-INSERT INTO [dbo].[IntegrationType]
-	(Title, IsDeleted, APIURL, Code)
-	values ('WorkFront', 0, '.attask-ondemand.com/attask/api', 'WorkFront')
+INSERT INTO [dbo].[IntegrationType] (Title, IsDeleted, APIURL, Code) values ('WorkFront', 0, '.attask-ondemand.com/attask/api', 'WorkFront')
 GO
 
 DECLARE @IntegrationTypeID int;
@@ -17,6 +15,7 @@ select  @IntegrationTypeID = IntegrationTypeId from [dbo].[IntegrationType]	wher
 
 --Insert Datatypes for mapping Gameplan Fields to WorkFront
 IF NOT EXISTS(SELECT * FROM [dbo].[GameplanDataType] WHERE [IntegrationTypeId] = @IntegrationTypeID)
+begin
 	insert into [dbo].[GameplanDataType]
 		values(@IntegrationTypeID, 'Global', 'Title', 'Name', 0,0,0) 
 	insert into [dbo].[GameplanDataType]
@@ -31,6 +30,7 @@ IF NOT EXISTS(SELECT * FROM [dbo].[GameplanDataType] WHERE [IntegrationTypeId] =
 		values(@IntegrationTypeID, 'Plan_Campaign_Program_Tactic', 'Cost', 'Cost (Budgeted)', 0,0,0) 
 	insert into [dbo].[GameplanDataType]
 		values(@IntegrationTypeID, 'Plan_Campaign_Program_Tactic', 'CostActual', 'Cost (Actual)', 0,0,0) 
+end
 GO
 			 
 DECLARE @IntegrationTypeID int;
@@ -39,10 +39,8 @@ select  @IntegrationTypeID = IntegrationTypeId from [dbo].[IntegrationType]	wher
 -- Add WorkFront integration attribute to IntegrationTypeAttribute
 --WorkFront api requires the company name in front of the uri
 IF NOT EXISTS(SELECT * FROM [dbo].[IntegrationTypeAttribute] WHERE [IntegrationTypeID] = @IntegrationTypeID AND [Attribute] = 'Company Name')
-	insert into [dbo].[IntegrationTypeAttribute]
-	values(@IntegrationTypeID, 'Company Name', 'textbox', 0)
+	insert into [dbo].[IntegrationTypeAttribute] values(@IntegrationTypeID, 'Company Name', 'textbox', 0)
 GO
-
 
 -- Add new column in Plan_Campaign_Program_Tactic called "IntegrationWorkFrontProjectID"
 --This is designed to contain WorkFront Project IDs to link the tactic to the project
