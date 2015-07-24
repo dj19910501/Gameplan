399dee5bc780fdf0eb9833a464f932338b3f5e23
Brad Gray <brad@hive9.com>
2015-07-24 17:43:49 -0500

    database scripts added for workfront integration changes

--- Database Scripts/07-2015/07-2015_Migration_Script_MRP.sql
+++ Database Scripts/07-2015/07-2015_Migration_Script_MRP.sql
@@ -47,3 +47,39 @@ GO
 IF NOT EXISTS(SELECT * FROM sys.columns WHERE [name] = 'IntegrationWorkFrontProjectID') 
 ALTER TABLE [dbo].[Plan_Campaign_Program_Tactic] 	ADD IntegrationWorkFrontProjectID nvarchar(50)
 GO
+
+-- Add new column in TacticType called "WorkFront Template"
+--Tactic Types will can now have WorkFront templates tied to them
+IF NOT EXISTS(SELECT * FROM sys.columns WHERE [name] = 'WorkFront Template') 
+ALTER TABLE [dbo].[TacticType] 	ADD [WorkFront Template] nvarchar(255)
+GO
+
+/****** Object:  Table [dbo].[IntegrationWorkFrontTemplates]    Script Date: 7/24/2015 5:24:25 PM ******/
+
+declare @tablecreate int = 0;
+
+IF (not EXISTS (SELECT * FROM sys.TABLES WHERE name = 'IntegrationWorkFrontTemplates'))
+begin
+set @tablecreate = 1
+exec('SET ANSI_NULLS ON')
+exec('if (@tablecreate =1)SET QUOTED_IDENTIFIER ON')
+exec('
+CREATE TABLE [dbo].[IntegrationWorkFrontTemplates](
+	[ID] [int] IDENTITY(1,1) NOT NULL,
+	[IntegrationInstanceId] [int] NOT NULL,
+	[TemplateId] [nvarchar](250) NOT NULL,
+	[Template Name] [nvarchar](250) NOT NULL,
+	[IsDeleted] [int] NOT NULL,
+ CONSTRAINT [PK_IntegrationWorkFrontTemplates] PRIMARY KEY CLUSTERED 
+(
+	[ID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+')
+exec('ALTER TABLE [dbo].[IntegrationWorkFrontTemplates]  WITH CHECK ADD  CONSTRAINT [FK_IntegrationInstanceId] FOREIGN KEY([IntegrationInstanceId])
+REFERENCES [dbo].[IntegrationInstance] ([IntegrationInstanceId])')
+exec('ALTER TABLE [dbo].[IntegrationWorkFrontTemplates] CHECK CONSTRAINT [FK_IntegrationInstanceId]')
+
+end
+
+
