1a17db0f40ab06713d5dfab136d52f7e01a1f348
unknown <dashrathp@.indusa.com>
2015-09-14 12:01:07 +0530

    Made minor change related to PL #1589.

--- Entities/Models/MRP.Designer.cs
+++ Entities/Models/MRP.Designer.cs
@@ -1,4 +1,4 @@
-﻿// Default code generation is disabled for model 'E:\Project\Gameplan\GPSeptember_3\Gameplan\Entities\Models\MRP.edmx'. 
+﻿// Default code generation is disabled for model 'E:\Project\Gameplan\GPSeptember_1\Gameplan\Entities\Models\MRP.edmx'. 
 // To enable default code generation, change the value of the 'Code Generation Strategy' designer
 // property to an alternate value. This property is available in the Properties Window when the model is
 // open in the designer.
