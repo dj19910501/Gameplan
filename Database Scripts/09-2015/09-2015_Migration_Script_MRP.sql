f13327b2973733a51fe6ccebacc23d073571aa7a
unknown <dashrathp@.indusa.com>
2015-09-28 17:21:24 +0530

    I have add script for Finance Tab related to PL #1587

--- Database Scripts/09-2015/09-2015_Migration_Script_MRP.sql
+++ Database Scripts/09-2015/09-2015_Migration_Script_MRP.sql
@@ -204,3 +204,26 @@ END
 Go
 
 
+-- ======================================================================================
+-- Created By : Dashrath Prajapati
+-- Created Date : 28/09/2015
+-- Description : Add New Finance Tab
+-- ======================================================================================
+
+IF (NOT EXISTS(SELECT * FROM [Menu_Application] WHERE code = 'FINANCE')) 
+BEGIN 
+    INSERT INTO [Menu_Application](ApplicationId,Code,Name,IsDisplayInMenu,SortOrder,ControllerName,ActionName,CreatedDate,CreatedBy,IsDeleted) 
+    VALUES('1C10D4B9-7931-4A7C-99E9-A158CE158951','FINANCE','Finance',1,4,'Finance','Index',GETDATE(),'F37A855C-9BF4-4A1F-AB7F-B21AF43EB2AF',0) 
+
+	UPDATE [Menu_Application] 
+    SET SortOrder = 5
+	WHERE Code = 'BOOST' 
+	AND ApplicationId ='1C10D4B9-7931-4A7C-99E9-A158CE158951'
+	AND IsDeleted = 0
+
+	UPDATE [Menu_Application] 
+    SET SortOrder = 6
+	WHERE Code = 'Report' 
+	AND ApplicationId ='1C10D4B9-7931-4A7C-99E9-A158CE158951'
+	AND IsDeleted = 0
+END 
