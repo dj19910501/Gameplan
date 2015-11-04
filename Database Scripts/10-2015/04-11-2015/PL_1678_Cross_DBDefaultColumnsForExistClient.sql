1d09d930e60387f9420bcd237d6b8a7bbff61d82
unknown <nishants@.indusa.com>
2015-11-04 19:51:32 +0530

    Hi, i made changes regarding add new table for custom columns, columns set and budget permission

--- Database Scripts/10-2015/04-11-2015/PL_1678_Cross_DBDefaultColumnsForExistClient.sql
+++ Database Scripts/10-2015/04-11-2015/PL_1678_Cross_DBDefaultColumnsForExistClient.sql
@@ -3,20 +3,6 @@ DECLARE @BDSAUTHDBName nvarchar(50) = 'BDSAuthDev'
 DECLARE @MRPDBName nvarchar(50) = 'MRPDev'
 
 DECLARE @SQLString nvarchar(max)=''
-
-SET @SQLString = @SQLString + N'INSERT INTO ' + @MRPDBName + '.dbo.Budget ([ClientId], [Name], [CreatedDate], [CreatedBy],[IsOther],[IsDeleted])
-SELECT C.ClientId,''Other'',GETDATE(),C.CreatedBy,1,0 FROM ' + @BDSAUTHDBName + '.dbo.Client C
-WHERE C.ClientId NOT IN (SELECT ClientId FROM ' + @MRPDBName + '.dbo.Budget WHERE IsOther = 1) AND C.CreatedBy IS NOT NULL'
-
-Exec(@SQLString)
-SET @SQLString = ''
-SET @SQLString = @SQLString + N'INSERT INTO ' + @MRPDBName + '.dbo.Budget_Detail ([BudgetId],[Name],[CreatedDate],[CreatedBy])
-SELECT MB.Id,''Other'',GETDATE(),MB.CreatedBy FROM ' + @MRPDBName + '.dbo.Budget MB WHERE IsOther = 1 AND id NOT IN 
-(SELECT BudgetId FROM ' + @MRPDBName + '.dbo.Budget_Detail) AND MB.CreatedBy IS NOT NULL'
-
-Exec(@SQLString)
-
--- Query for add Default columns in exist client
 Declare @SQLCreateTemp nvarchar(max)=''
 Declare @SQLDROPTemp nvarchar(max)=''
 Declare @SQLColumnSet nvarchar(max)=''
@@ -204,6 +190,6 @@ END
 
 SET @i=@i+1
 END;
--- End Columns on Budget Columns Table 
 
-GO
+
+
