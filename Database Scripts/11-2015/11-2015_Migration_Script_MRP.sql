4be912c2b1faa545ff3c437931112535f4e9e89c
unknown <dashrathp@.indusa.com>
2015-11-24 15:00:38 +0530

    Made change related to PL#1679 Observation 1,5

--- Database Scripts/11-2015/11-2015_Migration_Script_MRP.sql
+++ Database Scripts/11-2015/11-2015_Migration_Script_MRP.sql
@@ -3,8 +3,7 @@
 -- Description :Insert BudgetDetail Id into BudgetPermission table related to PL #1679
 -- ======================================================================================
 GO
-IF NOT EXISTS(SELECT id FROM [dbo].[Budget_Detail] WHERE Id NOT IN  (SELECT BudgetDetailId 
-     FROM [dbo].[Budget_Permission]))
+
 begin
 	INSERT INTO [dbo].[Budget_Permission] ([UserId],[BudgetDetailId],[CreatedDate],[CreatedBy],[PermisssionCode])
 Select [CreatedBy],[Id],GETDATE(),[CreatedBy],0 From [dbo].[Budget_Detail] Where Id NOT IN(
--- Database Scripts/11-2015/11-24-2015/PL#1679_InsertbudgetidtoBudgetPermission.sql
+++ Database Scripts/11-2015/11-24-2015/PL#1679_InsertbudgetidtoBudgetPermission.sql
@@ -1,11 +1,6 @@
-IF NOT EXISTS(SELECT id FROM [dbo].[Budget_Detail] WHERE Id NOT IN  (SELECT BudgetDetailId 
-     FROM [dbo].[Budget_Permission]))
-begin
+
 	INSERT INTO [dbo].[Budget_Permission] ([UserId],[BudgetDetailId],[CreatedDate],[CreatedBy],[PermisssionCode])
 Select [CreatedBy],[Id],GETDATE(),[CreatedBy],0 From [dbo].[Budget_Detail] Where Id NOT IN(
-Select BudgetDetailId From [dbo].[Budget_Permission]
-)
-end
-GO
+Select BudgetDetailId From [dbo].[Budget_Permission])
 
 
