880fffcaf157a8db37fd333980b78782d871b542
unknown <viralk@.indusa.com>
2015-12-04 13:36:38 +0530

    Remove old DB Script to create new Dec09 branch.

--- Database Scripts/11-2015/11-24-2015/PL#1679_InsertbudgetidtoBudgetPermission.sql
+++
@@ -1,6 +0,0 @@
-
-	INSERT INTO [dbo].[Budget_Permission] ([UserId],[BudgetDetailId],[CreatedDate],[CreatedBy],[PermisssionCode])
-Select [CreatedBy],[Id],GETDATE(),[CreatedBy],0 From [dbo].[Budget_Detail] Where Id NOT IN(
-Select BudgetDetailId From [dbo].[Budget_Permission])
-
-
