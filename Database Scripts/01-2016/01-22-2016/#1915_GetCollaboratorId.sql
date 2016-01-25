44bf7ec02ac32c22a99059c1b50203c0867b5a55
unknown <nishants@.indusa.com>
2016-01-25 11:40:07 +0530

    Hi I have made changes for GetCollaboratorId stored procedure.

--- Database Scripts/01-2016/01-22-2016/#1915_GetCollaboratorId.sql
+++ Database Scripts/01-2016/01-22-2016/#1915_GetCollaboratorId.sql
@@ -10,8 +10,8 @@ CREATE PROCEDURE GetCollaboratorId
 AS
 BEGIN
 SELECT tacComment.* From [dbo].[Plan_Campaign_Program_Tactic_Comment] TacComment
-CROSS APPLY(Select * From [dbo].[Plan_Campaign_Program_Tactic] Tactic WHERE TacComment.PlanTacticId=Tactic.PlanTacticId) Tactic
-CROSS APPLY(Select * From  [dbo].[Plan_Campaign_Program] Program WHERE Tactic.PlanProgramId=Program.[PlanProgramId])Program
-CROSS APPLY(Select * FROM  [dbo].[Plan_Campaign] Camp WHERE Program.[PlanCampaignId]=Camp.[PlanCampaignId])Camp
+CROSS APPLY(Select PlanTacticId,PlanProgramId From [dbo].[Plan_Campaign_Program_Tactic] Tactic WHERE TacComment.PlanTacticId=Tactic.PlanTacticId) Tactic
+CROSS APPLY(Select PlanProgramId,PlanCampaignId From  [dbo].[Plan_Campaign_Program] Program WHERE Tactic.PlanProgramId=Program.[PlanProgramId])Program
+CROSS APPLY(Select PlanCampaignId,Planid FROM  [dbo].[Plan_Campaign] Camp WHERE Program.[PlanCampaignId]=Camp.[PlanCampaignId])Camp
 Where Camp.Planid=@PlanId
 END
