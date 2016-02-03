d49280562318033d08b35e7f37bc109586879d32
unknown <komalr@.indusa.com>
2016-02-03 19:20:32 +0530

    Changes regarding script for 1943

--- Database Scripts/01-2016/01-2016_Migration_Script_MRP.sql
+++ Database Scripts/01-2016/01-2016_Migration_Script_MRP.sql
@@ -57,3 +57,14 @@ GO
 
 /* --------- End Script of PL ticket #1943 --------- */
 
+/* --------- Start Script of PL ticket #1943 --------- */
+-- Created by : Komal Rawal
+-- Created On : 2/3/2016
+-- Description : Update dependency Date for Plans of a particular client.
+-- Note: Parameters  @ClientId and @DependencyDate needs to be changed as per requirement
+Declare @ClientId uniqueidentifier = 'C251AB18-0683-4D1D-9F1E-06709D59FD53'
+Declare @DependencyDate datetime = '2016-01-05 19:59:00.417'
+Update [Plan] set DependencyDate = @DependencyDate where PlanId In (Select PlanId from [Plan] where ModelId IN ( Select ModelId from [Model] where ClientId = @ClientId and IsDeleted='0') and IsDeleted='0')
+
+/* --------- End Script of PL ticket #1943--------- */
+
