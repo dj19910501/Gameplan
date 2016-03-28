d7a7dd9862b70bd96ac250234d0b3fcfa18b5564
Nishant Sheth <nishants@indusa.com>
2016-03-28 12:39:17 +0530

    Hi i have made changes for database script regarding #1798

--- Database Scripts/03-23March-2016_Migration_Script_MRP.sql
+++ Database Scripts/03-23March-2016_Migration_Script_MRP.sql
@@ -75,6 +75,61 @@ ALTER COLUMN [Version] nvarchar(255)
 END
 GO
 
+IF OBJECT_ID('GetListPlanCampaignProgramTactic', 'P') IS NOT NULL
+BEGIN
+DROP PROC [dbo].[GetListPlanCampaignProgramTactic]
+END
+GO
+
+GO
+-- Created by nishant Sheth
+-- Created on :: 03-Feb-2016
+-- Desc :: Get list of Plans,Campaigns,Prgorams,Tactics
+CREATE PROCEDURE [dbo].[GetListPlanCampaignProgramTactic]
+ @PlanId nvarchar(max)=NULL,
+ @ClientId nvarchar(max)=NULL
+AS
+BEGIN
+SET NOCOUNT ON;
+
+IF OBJECT_ID('tempdb..#tempPlanId') IS NOT NULL
+    DROP TABLE #tempPlanId
+SELECT val into #tempPlanId FROM dbo.comma_split(@Planid, ',')
+
+IF OBJECT_ID('tempdb..#tempClientId') IS NOT NULL
+    DROP TABLE #tempClientId
+SELECT val into #tempClientId FROM dbo.comma_split(@ClientId, ',')
+
+-- Plan Details
+SELECT * FROM [Plan] AS [Plan] WITH (NOLOCK) 
+CROSS APPLY (SELECT ModelId,ClientId From [Model]  WITH (NOLOCK) WHERE [Model].ModelId = [Plan].ModelId AND ClientId IN (SELECT val From #tempClientId)) [Model]
+WHERE [Plan].PlanId IN (SELECT val From #tempPlanId)
+ AND [Plan].IsDeleted=0 
+
+-- Campaign Details
+SELECT Campaign.* FROM Plan_Campaign Campaign
+CROSS APPLY (SELECT PlanId,ModelId From [Plan] WITH (NOLOCK) WHERE [Plan].PlanId IN (SELECT val From #tempPlanId)  AND [Plan].PlanId=Campaign.PlanId AND [Plan].IsDeleted=0 ) [Plan]
+CROSS APPLY (SELECT ModelId,ClientId From [Model]  WITH (NOLOCK) WHERE [Model].ModelId = [Plan].ModelId AND ClientId IN (SELECT val From #tempClientId)) [Model]
+WHERE Campaign.IsDeleted=0
+
+-- Program Details
+SELECT Program.*,[Plan].PlanId FROM Plan_Campaign_Program Program WITH (NOLOCK)
+CROSS APPLY (SELECT PlanCampaignId,PlanId FROM Plan_Campaign AS Campaign WITH (NOLOCK) WHERE Campaign.PlanCampaignId=Program.PlanCampaignId AND Campaign.IsDeleted=0) Campaign 
+CROSS APPLY (SELECT PlanId,ModelId From [Plan] WITH (NOLOCK) WHERE [Plan].PlanId IN (SELECT val From #tempPlanId)  AND [Plan].PlanId=Campaign.PlanId AND [Plan].IsDeleted=0 ) [Plan]
+CROSS APPLY (SELECT ModelId,ClientId From [Model]  WITH (NOLOCK) WHERE [Model].ModelId = [Plan].ModelId AND ClientId IN (SELECT val From #tempClientId)) [Model]
+WHERE Program.IsDeleted=0
+
+-- Tactic Details
+SELECT Tactic.*,[Plan].PlanId,[Campaign].PlanCampaignId,[TacticType].[Title] AS 'TacticTypeTtile',[TacticType].[ColorCode],[Plan].[Year] AS 'PlanYear',[Plan].ModelId FROM Plan_Campaign_Program_Tactic AS Tactic WITH (NOLOCK) 
+CROSS APPLY (SELECT PlanProgramId,PlanCampaignId FROM Plan_Campaign_Program AS Program WITH (NOLOCK) WHERE Program.PlanProgramId=Tactic.PlanProgramId AND Program.IsDeleted=0) Program
+CROSS APPLY (SELECT PlanCampaignId,PlanId FROM Plan_Campaign AS Campaign WITH (NOLOCK) WHERE Campaign.PlanCampaignId=Program.PlanCampaignId AND Campaign.IsDeleted=0) Campaign 
+CROSS APPLY (SELECT PlanId,ModelId,[Year] From [Plan] WITH (NOLOCK) WHERE [Plan].PlanId IN (SELECT val From #tempPlanId)  AND [Plan].PlanId=Campaign.PlanId AND [Plan].IsDeleted=0 ) [Plan]
+CROSS APPLY (SELECT ModelId,ClientId From [Model]  WITH (NOLOCK) WHERE [Model].ModelId = [Plan].ModelId AND ClientId IN (SELECT val From #tempClientId)) [Model]
+OUTER APPLY (SELECT [TacticTypeId],[Title],[ColorCode] FROM [TacticType] WITH (NOLOCK) Where [Tactic].TacticTypeId=TacticType.TacticTypeId AND IsDeleted=0) [TacticType]
+WHERE Tactic.IsDeleted=0
+END
+
+GO
 
 /* ------------- End - Related to PL ticket #1449 ------------- */
 
@@ -96,8 +151,8 @@ GO
 
 declare @version nvarchar(255)
 declare @release nvarchar(255)
-set @release = 'Mar23.2016'
-set @version = 'Mar23.2016.1'
+set @release = 'Mar30.2016'
+set @version = 'Mar30.2016.1'
 declare @date as datetime
 set @date = getutcdate()
 
