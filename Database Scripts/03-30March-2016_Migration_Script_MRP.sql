9bf5f33c5535ecaf8faee545ef43aa5d5e800356
unknown <akashdeepk@.indusa.com>
2016-03-30 13:41:26 +0530

    Brad's script merged in Migration script.
    -Akashdeep Kadia

--- Database Scripts/03-30March-2016_Migration_Script_MRP.sql
+++ Database Scripts/03-30March-2016_Migration_Script_MRP.sql
@@ -1,3 +1,26 @@
+/* ------------- Start - Related to PL ticket #2070 ------------- 
+Created By: Brad Gray
+Created Date: 03/29/2016
+Description: Remove erroneous 'Campaign End Date' Custom fields and insert 'CampaignEndDate' custom field
+*/
+
+
+  DECLARE @inst1 as int = (SELECT IntegrationTypeId FROM [dbo].[IntegrationType] where Title = 'WorkFront')
+  if exists(select * from  [dbo].[GameplanDataType] where IntegrationTypeId = @inst1 AND ActualFieldName = 'Campaign End Date')
+  begin 
+
+DELETE m
+FROM [dbo].[IntegrationInstanceDataTypeMapping] m
+INNER JOIN [dbo].[GameplanDataType] t
+  on t.ActualFieldName = 'Campaign End Date' and t.IntegrationTypeId = @inst1 and t.GameplanDataTypeId = m.GameplanDataTypeId
+ delete from  [dbo].[GameplanDataType]  where IntegrationTypeId = @inst1 AND ActualFieldName = 'Campaign End Date'
+  end
+
+ if not exists(select * from  [dbo].[GameplanDataType] where IntegrationTypeId = @inst1 AND ActualFieldName = 'CampaignEndDate')
+ insert into [dbo].[GameplanDataType] values(@inst1,'Plan_Campaign', 'CampaignEndDate', 'Campaign End Date', 0,0,0)
+/* ------------- End - Related to PL ticket #2070 ------------- */
+
+
 /* ------------- Start - Related to PL ticket #2083 & #2084 ------------- 
 Created By: Brad Gray
 Created Date: 03/26/2016
