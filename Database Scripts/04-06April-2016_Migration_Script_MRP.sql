054781f8e1b698f400b03248de4b1ca97404f7fc
unknown <viralk@.indusa.com>
2016-03-31 14:56:21 +0530

    I have created stored procedure to full fill the points defined in ticket description.

--- Database Scripts/04-06April-2016_Migration_Script_MRP.sql
+++ Database Scripts/04-06April-2016_Migration_Script_MRP.sql
@@ -1,3 +1,91 @@
+-- =============================================
+-- Author: Viral 
+-- Create date: 03/31/2016
+-- Description:	Update Tactic table with IntegrationInstanceTacticId field & insert Create/Update comment to Plan_Campaign_Program_Tactic_Comment table for Tactic & Linked tactic.
+-- =============================================
+/****** Object:  StoredProcedure [dbo].[UpdateTacticInstanceTacticId_Comment]    Script Date: 03/31/2016 2:49:19 PM ******/
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[UpdateTacticInstanceTacticId_Comment]') AND type in (N'P', N'PC'))
+DROP PROCEDURE [dbo].[UpdateTacticInstanceTacticId_Comment]
+GO
+/****** Object:  StoredProcedure [dbo].[UpdateTacticInstanceTacticId_Comment]    Script Date: 03/31/2016 2:49:19 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[UpdateTacticInstanceTacticId_Comment]') AND type in (N'P', N'PC'))
+BEGIN
+EXEC dbo.sp_executesql @statement = N'CREATE PROCEDURE [dbo].[UpdateTacticInstanceTacticId_Comment] AS' 
+END
+GO
+
+ALTER PROCEDURE [dbo].[UpdateTacticInstanceTacticId_Comment]
+	@strCreatedTacIds nvarchar(max),
+	@strUpdatedTacIds nvarchar(max),
+	@strUpdateComment nvarchar(max),
+	@strCreateComment nvarchar(max),
+	@isAutoSync bit='0',
+	@userId uniqueidentifier
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+	Declare @strAllPlanTacIds nvarchar(max)=''
+	IF(@strCreatedTacIds<>'')
+	BEGIN
+		SET @strAllPlanTacIds = @strCreatedTacIds
+	END
+	IF(@strUpdatedTacIds<>'')
+	BEGIN
+		IF(@strAllPlanTacIds<>'')
+		BEGIN
+			SET @strAllPlanTacIds = @strAllPlanTacIds+','+@strUpdatedTacIds
+		END
+		ELSE
+		BEGIN
+			SET @strAllPlanTacIds = @strUpdatedTacIds
+		END
+	END
+	-- update IntegrationInstanceTacticId for linked tactic 
+	Update  tac2 set tac2.IntegrationInstanceTacticId=tac1.IntegrationInstanceTacticId,tac2.TacticCustomName=tac1.TacticCustomName,tac2.LastSyncDate=tac1.LastSyncDate,tac2.ModifiedDate = tac1.ModifiedDate,tac2.ModifiedBy = tac1.ModifiedBy from Plan_Campaign_Program_Tactic tac1
+    join Plan_Campaign_Program_Tactic tac2 on tac1.LinkedTacticId=tac2.PlanTacticId 
+    where tac1.PlanTacticId IN (Select cast(val as int) from dbo.[comma_split](@strAllPlanTacIds, ','))
+
+	IF(@isAutoSync =0)
+	BEGIN
+		IF OBJECT_ID('tempdb..#tmp_Plan_Campaign_Program_Tactic_Comment') IS NOT NULL 
+		BEGIN
+			DROP TABLE #tmp_Plan_Campaign_Program_Tactic_Comment
+		END
+		
+		Create Table #tmp_Plan_Campaign_Program_Tactic_Comment(CommentId int,Tacticid int)
+		
+		-- Insert comment for PlanTactic
+		Insert Into Plan_Campaign_Program_Tactic_Comment
+		OUTPUT inserted.PlanTacticCommentId,inserted.PlanTacticId into #tmp_Plan_Campaign_Program_Tactic_Comment
+		SElect PlanTacticId,@strCreateComment,GETDATE(),@userId,null,null from Plan_Campaign_Program_Tactic where PlanTacticId In (Select cast(val as int) from dbo.[comma_split](@strCreatedTacIds, ','))
+		UNION
+		SElect PlanTacticId,@strUpdateComment,GETDATE(),@userId,null,null from Plan_Campaign_Program_Tactic where PlanTacticId In (Select cast(val as int) from dbo.[comma_split](@strUpdatedTacIds, ','))
+		
+		-- Insert comment for linked Tactic
+		Insert Into Plan_Campaign_Program_Tactic_Comment
+		Select tac2.PlanTacticId,cmnt.Comment,cmnt.CreatedDate,cmnt.CreatedBy,cmnt.PlanProgramId,cmnt.PlanCampaignId from #tmp_Plan_Campaign_Program_Tactic_Comment as tmpComment
+		join Plan_Campaign_Program_Tactic tac1 on tac1.PlanTacticId = tmpComment.TacticId
+		join Plan_Campaign_Program_Tactic tac2 on tac1.LinkedTacticId=tac2.PlanTacticId 
+		join Plan_Campaign_Program_Tactic_Comment as cmnt on tmpComment.CommentId = cmnt.PlanTacticCommentId and tmpComment.TacticId = cmnt.PlanTacticId
+		where tac1.PlanTacticId IN (Select cast(val as int) from dbo.[comma_split](@strAllPlanTacIds, ','))
+
+		IF OBJECT_ID('tempdb..#tmp_Plan_Campaign_Program_Tactic_Comment') IS NOT NULL 
+		BEGIN
+			DROP TABLE #tmp_Plan_Campaign_Program_Tactic_Comment
+		END
+		
+	END
+    
+END
+
+GO
+
 -- Added By : Maitri Gandhi
 -- Added Date : 2/22/2016
 -- Description :Ensure versioning table exists & Update versioning table with script version
