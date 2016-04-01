4006e0d97ba93dd3017655ae0fdec5837affcdc5
unknown <nishants@.indusa.com>
2016-04-01 17:33:34 +0530

    Hi i have made changes for #1798 script changes

--- Database Scripts/04-06April-2016_Migration_Script_MRP.sql
+++ Database Scripts/04-06April-2016_Migration_Script_MRP.sql
@@ -86,6 +86,111 @@ END
 
 GO
 
+-- =============================================
+-- Author: Nishant 
+-- Create date: 04/01/2016
+-- Description: Get list of plan, program, tactic
+-- =============================================
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[GetListPlanCampaignProgramTactic]') AND type in (N'P', N'PC'))
+DROP PROCEDURE [dbo].[GetListPlanCampaignProgramTactic]
+GO
+-- Created by nishant Sheth
+-- Created on :: 03-Feb-2016
+-- Desc :: Get list of Plans,Campaigns,Prgorams,Tactics
+-- EXEC [dbo].[GetListPlanCampaignProgramTactic] '12912','464EB808-AD1F-4481-9365-6AADA15023BD'
+CREATE PROCEDURE [dbo].[GetListPlanCampaignProgramTactic]
+ @PlanId nvarchar(max)=NULL,
+ @ClientId nvarchar(max)=NULL
+AS
+BEGIN
+SET NOCOUNT ON;
+
+IF OBJECT_ID('tempdb..#tempPlanId') IS NOT NULL
+    DROP TABLE #tempPlanId
+SELECT val into #tempPlanId FROM dbo.comma_split(@Planid, ',')
+
+IF OBJECT_ID('tempdb..#tempClientId') IS NOT NULL
+    DROP TABLE #tempClientId
+SELECT val into #tempClientId FROM dbo.comma_split(@ClientId, ',')
+
+-- Plan Details
+SELECT * FROM [Plan] AS [Plan] WITH (NOLOCK) 
+CROSS APPLY (SELECT ModelId,ClientId From [Model]  WITH (NOLOCK) WHERE [Model].ModelId = [Plan].ModelId AND ClientId IN (SELECT val From #tempClientId)) [Model]
+WHERE [Plan].PlanId IN (SELECT val From #tempPlanId)
+ AND [Plan].IsDeleted=0 
+
+-- Campaign Details
+SELECT Campaign.* FROM Plan_Campaign Campaign
+CROSS APPLY (SELECT PlanId,ModelId From [Plan] WITH (NOLOCK) WHERE [Plan].PlanId IN (SELECT val From #tempPlanId)  AND [Plan].PlanId=Campaign.PlanId AND [Plan].IsDeleted=0 ) [Plan]
+CROSS APPLY (SELECT ModelId,ClientId From [Model]  WITH (NOLOCK) WHERE [Model].ModelId = [Plan].ModelId AND ClientId IN (SELECT val From #tempClientId)) [Model]
+WHERE Campaign.IsDeleted=0
+
+-- Program Details
+SELECT Program.*,[Plan].PlanId FROM Plan_Campaign_Program Program WITH (NOLOCK)
+CROSS APPLY (SELECT PlanCampaignId,PlanId FROM Plan_Campaign AS Campaign WITH (NOLOCK) WHERE Campaign.PlanCampaignId=Program.PlanCampaignId AND Campaign.IsDeleted=0) Campaign 
+CROSS APPLY (SELECT PlanId,ModelId From [Plan] WITH (NOLOCK) WHERE [Plan].PlanId IN (SELECT val From #tempPlanId)  AND [Plan].PlanId=Campaign.PlanId AND [Plan].IsDeleted=0 ) [Plan]
+CROSS APPLY (SELECT ModelId,ClientId From [Model]  WITH (NOLOCK) WHERE [Model].ModelId = [Plan].ModelId AND ClientId IN (SELECT val From #tempClientId)) [Model]
+WHERE Program.IsDeleted=0
+
+-- Tactic Details
+SELECT Tactic.*,[Plan].PlanId,[Campaign].PlanCampaignId,[TacticType].[Title] AS 'TacticTypeTtile',[TacticType].[ColorCode],[Plan].[Year] AS 'PlanYear',[Plan].ModelId, 
+[Campaign].Title AS 'CampaignTitle',[Program].Title AS 'ProgramTitle',[Plan].Title AS 'PlanTitle', [Stage].Title AS 'StageTitle',[Plan].[Status] AS 'PlanStatus'
+FROM Plan_Campaign_Program_Tactic AS Tactic WITH (NOLOCK) 
+CROSS APPLY (SELECT PlanProgramId,PlanCampaignId,Title FROM Plan_Campaign_Program AS Program WITH (NOLOCK) WHERE Program.PlanProgramId=Tactic.PlanProgramId AND Program.IsDeleted=0) Program
+CROSS APPLY (SELECT PlanCampaignId,PlanId,Title FROM Plan_Campaign AS Campaign WITH (NOLOCK) WHERE Campaign.PlanCampaignId=Program.PlanCampaignId AND Campaign.IsDeleted=0) Campaign 
+CROSS APPLY (SELECT PlanId,ModelId,[Year],Title,[Status] From [Plan] WITH (NOLOCK) WHERE [Plan].PlanId IN (SELECT val From #tempPlanId)  AND [Plan].PlanId=Campaign.PlanId AND [Plan].IsDeleted=0 ) [Plan]
+CROSS APPLY (SELECT ModelId,ClientId From [Model]  WITH (NOLOCK) WHERE [Model].ModelId = [Plan].ModelId AND ClientId IN (SELECT val From #tempClientId)) [Model]
+OUTER APPLY (SELECT [TacticTypeId],[Title],[ColorCode] FROM [TacticType] WITH (NOLOCK) Where [Tactic].TacticTypeId=TacticType.TacticTypeId AND IsDeleted=0) [TacticType]
+OUTER APPLY (SELECT [StageId],[Title] FROM Stage WHERE [Tactic].StageId=Stage.StageId) Stage
+WHERE Tactic.IsDeleted=0
+END
+
+GO
+
+-- =============================================
+-- Author: Nishant 
+-- Create date: 04/01/2016
+-- Description: Get list Custom fields and Custom field entity list
+-- =============================================
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[GetCustomFieldEntityList]') AND type in (N'P', N'PC'))
+DROP PROCEDURE [dbo].[GetCustomFieldEntityList]
+GO
+-- Created by nishant Sheth
+-- Created on :: 03-Mar-2016
+-- Desc :: Get list of entity values
+-- EXEC GetCustomFieldEntityList 42,'Tactic','464eb808-ad1f-4481-9365-6aada15023bd'
+CREATE PROCEDURE [dbo].[GetCustomFieldEntityList]
+@CustomTypeId int = null
+,@EntityType nvarchar(50) = ''
+,@ClientId nvarchar(250)=null
+AS
+BEGIN
+SET NOCOUNT ON;
+
+SELECT CustomField.* FROM CustomField (NOLOCK) WHERE 
+IsDeleted=0
+AND CustomFieldId=CASE WHEN @CustomTypeId IS NULL THEN CustomFieldId ELSE @CustomTypeId END
+AND EntityType= CASE WHEN @EntityType ='' THEN EntityType ELSE @EntityType END
+AND ClientId= CASE WHEN @ClientId IS NULL THEN ClientId ELSE @ClientId END
+
+
+SELECT CustomField_Entity.EntityId,CustomField_Entity.CustomFieldId,CustomField_Entity.Value
+,CustomField_Entity.CreatedBy
+,CustomField_Entity.CustomFieldEntityId
+ FROM CustomField_Entity (NOLOCK) 
+CROSS APPLY(
+SELECT CustomField.CustomFieldId FROM CustomField (NOLOCK) WHERE 
+IsDeleted=0
+AND CustomFieldId=CASE WHEN @CustomTypeId IS NULL THEN CustomFieldId ELSE @CustomTypeId END
+AND EntityType = CASE WHEN @EntityType ='' THEN EntityType ELSE @EntityType END
+AND ClientId = CASE WHEN @ClientId IS NULL THEN ClientId ELSE @ClientId END
+AND CustomField.CustomFieldId=CustomField_Entity.CustomFieldId
+) CustomField 
+
+END
+
+GO
+
 -- Added By : Maitri Gandhi
 -- Added Date : 2/22/2016
 -- Description :Ensure versioning table exists & Update versioning table with script version
