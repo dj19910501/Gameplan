caa09f02507cf133c42c19af1d5e89e307fa1be8
unknown <akashdeepk@.indusa.com>
2016-04-19 16:53:51 +0530

    Pending checking for PL Ticket 2102

--- Database Scripts/04-2016_Migration_Script_MRP.sql
+++ Database Scripts/04-2016_Migration_Script_MRP.sql
@@ -1,3 +1,44 @@
+-- =============================================
+-- Author: Nishant 
+-- Create date: 04/06/2016
+-- Description: 
+-- =============================================
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[SplitString]'))
+DROP FUNCTION [dbo].[SplitString]
+GO
+Create FUNCTION [dbo].[SplitString]
+(    
+      @Input NVARCHAR(MAX),
+      @Character CHAR(1)
+)
+RETURNS @Output TABLE (
+      Item NVARCHAR(1000)
+)
+AS
+BEGIN
+      DECLARE @StartIndex INT, @EndIndex INT
+ 
+      SET @StartIndex = 1
+      IF SUBSTRING(@Input, LEN(@Input) - 1, LEN(@Input)) <> @Character
+      BEGIN
+            SET @Input = @Input + @Character
+      END
+ 
+      WHILE CHARINDEX(@Character, @Input) > 0
+      BEGIN
+            SET @EndIndex = CHARINDEX(@Character, @Input)
+           
+            INSERT INTO @Output(Item)
+            SELECT SUBSTRING(@Input, @StartIndex, @EndIndex - 1)
+           
+            SET @Input = SUBSTRING(@Input, @EndIndex + 1, LEN(@Input))
+      END
+ 
+      RETURN
+END
+
+
+
 GO
 -- =============================================
 -- Author: Nishant 
