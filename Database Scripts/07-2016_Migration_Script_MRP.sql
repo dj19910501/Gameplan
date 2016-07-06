2a8e37e987aad8998d66bf8ef921274041d21f7d
John Zhang <jzhang@hive9.com>
2016-07-06 10:44:43 -0500

    #2341 added the improved stored procedure to July migration script.

--- Database Scripts/07-2016_Migration_Script_MRP.sql
+++ Database Scripts/07-2016_Migration_Script_MRP.sql
@@ -1,3 +1,56 @@
+
+/****** Object:  StoredProcedure [dbo].[spViewByDropDownList]    Script Date: 7/5/2016 2:11:36 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+-- =============================================
+-- Author:		<Author,,JZhang>
+-- Create date: <Create Date,05-July-2016,>
+-- Description:	<This is a rewrite of the orginal proc for performance reason. Using in memory table reduces time from 900 ms to 40 ms on average>
+-- =============================================
+ALTER PROCEDURE  [dbo].[spViewByDropDownList] 
+	-- Add the parameters for the stored procedure here
+	@PlanId NVARCHAR(max),
+	@ClientId NVARCHAR(50)
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+	DECLARE @tblCustomerFieldIDs TABLE ( EntityType NVARCHAR(120), EntityID INT, CustomFieldID INT) 
+
+	INSERT INTO @tblCustomerFieldIDs SELECT 'Campaign', A.PlanCampaignId, B.CustomFieldId 
+	FROM Plan_Campaign A CROSS APPLY (  SELECT B.CustomFieldId 
+										FROM CustomField_Entity B 
+										WHERE B.EntityId = A.PlanCampaignId AND A.IsDeleted=0 AND A.PlanId in ( SELECT val FROM dbo.comma_split(@Planid, ','))) B
+
+	INSERT INTO @tblCustomerFieldIDs SELECT 'Program', A.PlanProgramId, B.CustomFieldId 
+	FROM Plan_Campaign_Program A CROSS APPLY (	SELECT B.CustomFieldId  
+												FROM CustomField_Entity B 
+												WHERE B.EntityId = A.PlanProgramId AND A.IsDeleted=0 AND A.PlanCampaignId in(SELECT EntityId FROM @tblCustomerFieldIDs WHERE EntityType = 'Campaign')) B 
+
+	INSERT INTO @tblCustomerFieldIDs SELECT 'Tactic', A.PlanTacticId, B.CustomFieldId 
+	FROM Plan_Campaign_Program_Tactic A CROSS APPLY (	SELECT B.CustomFieldId 
+														FROM CustomField_Entity B 
+														WHERE B.EntityId = A.PlanTacticId AND A.IsDeleted=0 AND A.PlanProgramId in(SELECT EntityId FROM @tblCustomerFieldIDs WHERE EntityType = 'Program')) B
+
+	SELECT DISTINCT(A.Name) AS [Text],A.EntityType +'Custom'+ Cast(A.CustomFieldId as nvarchar(50)) as Value 
+	FROM CustomField A CROSS APPLY (	SELECT B.CustomFieldTypeId,B.Name 
+										FROM CustomFieldType B 
+										WHERE A.CustomFieldTypeId = B.CustomFieldTypeId) B CROSS APPLY (SELECT C.CustomFieldID 
+																										FROM @tblCustomerFieldIDs C 
+																										WHERE C.CustomFieldID = A.CustomFieldId) C 
+										
+	WHERE A.ClientId=@ClientId AND A.IsDeleted=0 AND A.IsDisplayForFilter=1 AND A.EntityType IN ('Tactic','Campaign','Program') and B.Name='DropDownList' 
+	ORDER BY Value DESC 
+
+ END
+
+GO
+
 IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[Plan_Budget_Cost_Actual_Detail]') AND type in (N'P', N'PC'))
 BEGIN
 EXEC dbo.sp_executesql @statement = N'CREATE PROCEDURE [dbo].[Plan_Budget_Cost_Actual_Detail] AS' 
