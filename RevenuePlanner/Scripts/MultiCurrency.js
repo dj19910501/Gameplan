3b1957e1cb20db2fef257abbb6bd6b61ae16bab4
unknown <nishants@.indusa.com>
2016-08-12 18:24:30 +0530

    Hi i have made changes regarding to set global varibles for Currency symbol, Exchange rates and get currency url PL #2496.

--- RevenuePlanner/Scripts/MultiCurrency.js
+++ RevenuePlanner/Scripts/MultiCurrency.js
@@ -1,7 +1,4 @@
 ﻿// Add By Nishant Sheth
-// Declare Global variables for currency symbols and plan currencies rates etc.
-var CurrencySybmol = '$';
-var PlanExchangeRate = 1;
 
 $(document).ready(function () {
     setCurrencyDetails();
@@ -10,18 +7,24 @@ $(document).ready(function () {
 // Add By Nishant Sheth // Get list of plan currency details 
 function setCurrencyDetails()
 {
-    $.ajax({
-        type: 'get',
-        url: '/Currency/GetPlanCurrencyDetail',
-        success: function (data) {
-            if (data != null && data != '') {
-                if (data.CurrencySymbol != null && data.CurrencySymbol != '') {
-                    CurrencySybmol = data.CurrencySymbol;
-                }
-                if (data.ExchangeRate != null && data.ExchangeRate != '') {
-                    PlanExchangeRate = data.ExchangeRate;
+    if (GetCurrencyURL != undefined) {
+        $.ajax({
+            type: 'get',
+            url: GetCurrencyURL,
+            success: function (data) {
+                if (data != null && data != '') {
+                    if (data.CurrencySymbol != null && data.CurrencySymbol != '') {
+                        if (CurrencySybmol != undefined) {
+                            CurrencySybmol = data.CurrencySymbol;
+                        }
+                    }
+                    if (data.ExchangeRate != null && data.ExchangeRate != '') {
+                        if (PlanExchangeRate != undefined) {
+                            PlanExchangeRate = data.ExchangeRate;
+                        }
+                    }
                 }
             }
-        }
-    });
+        });
+    }
 }
