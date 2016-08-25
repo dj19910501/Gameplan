91df9624198718ba8f19d04a862da24a9e34387c
Viral Kadiya <viral.kadiya@indusa.com>
2016-08-25 17:23:54 +0530

    I have made script change related to PL ticket #2537.

--- Database Scripts/08-2016_Migration_Script_MRP.sql
+++ Database Scripts/08-2016_Migration_Script_MRP.sql
@@ -3371,7 +3371,7 @@ GO
 -- Description :Added "LineItemTypeId" column for LineItem query.
 
 
-/****** Object:  StoredProcedure [dbo].[Plan_Budget_Cost_Actual_Detail]    Script Date: 08/24/2016 1:34:26 PM ******/
+/****** Object:  StoredProcedure [dbo].[Plan_Budget_Cost_Actual_Detail]    Script Date: 08/25/2016 5:16:22 PM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -3650,6 +3650,7 @@ ELSE
 ,[Y1],[Y2],[Y3],[Y4],[Y5],[Y6],[Y7],[Y8],[Y9],[Y10],[Y11],[Y12],[Y1] as [CY1],[Y2] as [CY2],[Y3] as [CY3],[Y4] as [CY4],[Y5] as [CY5],[Y6] as [CY6],[Y7] as [CY7],[Y8] as [CY8],[Y9] as [CY9],[Y10] as [CY10],[Y11] as [CY11],[Y12] as [CY12]
 ,ISNULL([Y1],0)+ISNULL([Y2],0)+ISNULL([Y3],0)+ISNULL([Y4],0)+ISNULL([Y5],0)+ISNULL([Y6],0)+ISNULL([Y7],0)+ISNULL([Y8],0)+ISNULL([Y9],0)+ISNULL([Y10],0)+ISNULL([Y11],0)+ISNULL([Y12],0) TotalBudgetSum
 ,0 TotalCostSum
+,0 as LineItemTypeId
  FROM
 			(
 			SELECT 
@@ -3692,6 +3693,7 @@ SELECT Id
 ,[Y1],[Y2],[Y3],[Y4],[Y5],[Y6],[Y7],[Y8],[Y9],[Y10],[Y11],[Y12],[Y1] as [CY1],[Y2] as [CY2],[Y3] as [CY3],[Y4] as [CY4],[Y5] as [CY5],[Y6] as [CY6],[Y7] as [CY7],[Y8] as [CY8],[Y9] as [CY9],[Y10] as [CY10],[Y11] as [CY11],[Y12] as [CY12]
 ,ISNULL([Y1],0)+ISNULL([Y2],0)+ISNULL([Y3],0)+ISNULL([Y4],0)+ISNULL([Y5],0)+ISNULL([Y6],0)+ISNULL([Y7],0)+ISNULL([Y8],0)+ISNULL([Y9],0)+ISNULL([Y10],0)+ISNULL([Y11],0)+ISNULL([Y12],0) TotalBudgetSum
 ,0 TotalCostSum
+,0 as LineItemTypeId
  FROM
 (
  
@@ -3732,7 +3734,9 @@ SELECT Id
 ,Cost
 ,[Y1],[Y2],[Y3],[Y4],[Y5],[Y6],[Y7],[Y8],[Y9],[Y10],[Y11],[Y12],[Y1] as [CY1],[Y2] as [CY2],[Y3] as [CY3],[Y4] as [CY4],[Y5] as [CY5],[Y6] as [CY6],[Y7] as [CY7],[Y8] as [CY8],[Y9] as [CY9],[Y10] as [CY10],[Y11] as [CY11],[Y12] as [CY12]
 ,ISNULL([Y1],0)+ISNULL([Y2],0)+ISNULL([Y3],0)+ISNULL([Y4],0)+ISNULL([Y5],0)+ISNULL([Y6],0)+ISNULL([Y7],0)+ISNULL([Y8],0)+ISNULL([Y9],0)+ISNULL([Y10],0)+ISNULL([Y11],0)+ISNULL([Y12],0) TotalBudgetSum 
-,0 TotalCostSum FROM
+,0 TotalCostSum
+,0 as LineItemTypeId
+ FROM
 (
 SELECT   CAST(PG.PlanProgramId as NVARCHAR(20)) Id,
 		 'cp_'+CAST(PG.PlanProgramId as NVARCHAR(20)) ActivityId
@@ -3774,6 +3778,7 @@ select Id
 
 ,ISNULL([CY1],0)+ISNULL([CY2],0)+ISNULL([CY3],0)+ISNULL([CY4],0)+ISNULL([CY5],0)+ISNULL([CY6],0)+ISNULL([CY7],0)+ISNULL([CY8],0)+ISNULL([CY9],0)+ISNULL([CY10],0)+ISNULL([CY11],0)+ISNULL([CY12],0) TotalBudgetSum
 ,ISNULL([Y1],0)+ISNULL([Y2],0)+ISNULL([Y3],0)+ISNULL([Y4],0)+ISNULL([Y5],0)+ISNULL([Y6],0)+ISNULL([Y7],0)+ISNULL([Y8],0)+ISNULL([Y9],0)+ISNULL([Y10],0)+ISNULL([Y11],0)+ISNULL([Y12],0)  TotalCostSum 
+,0 as LineItemTypeId
 FROM
 (
  
@@ -3827,6 +3832,7 @@ SELECT Id
 ,0 [Y1],0 [Y2],0 [Y3],0 [Y4],0 [Y5],0 [Y6],0 [Y7],0 [Y8],0 [Y9],0 [Y10],0 [Y11],0 [Y12],[CY1],[CY2],[CY3],[CY4],[CY5],[CY6],[CY7],[CY8],[CY9],[CY10],[CY11],[CY12]
 ,ISNULL([CY1],0)+ISNULL([CY2],0)+ISNULL([CY3],0)+ISNULL([CY4],0)+ISNULL([CY5],0)+ISNULL([CY6],0)+ISNULL([CY7],0)+ISNULL([CY8],0)+ISNULL([CY9],0)+ISNULL([CY10],0)+ISNULL([CY11],0)+ISNULL([CY12],0) TotalBudgetSum
 ,0 TotalCostSum 
+,LineItemTypeId
 FROM
 (
 SELECT 
@@ -3843,6 +3849,7 @@ SELECT
 		,CASE WHEN PT.[Status] in ('Approved','In-Progress','Complete') THEN 1 ELSE 0 END IsAfterApproved
 		,PLC.Value
 		,'C'+PLC.period as period 
+		,PL.LineItemTypeId as LineItemTypeId
 	FROM Plan_Campaign_Program_Tactic_LineItem PL
 	INNER JOIN Plan_Campaign_Program_Tactic PT ON PT.PlanTacticId=PL.PlanTacticId
 	INNER JOIN Plan_Campaign_Program PPG ON PT.PlanProgramId=PPG.PlanProgramId
@@ -3862,7 +3869,9 @@ For Period in ([CY1], [CY2], [CY3], [CY4],[CY5], [CY6], [CY7], [CY8],[CY9], [CY1
 END
 
 
+
 GO
+
 
 
 
