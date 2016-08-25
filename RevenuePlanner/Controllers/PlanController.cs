bc2a2a09bacc83a635a7055a6906053b810bb0d2
rahul.shah@indusa.com <rahul.shah@indusa.com>
2016-08-25 19:25:47 +0530

    made changes regarding PL #2499. observation 4

--- RevenuePlanner/Controllers/PlanController.cs
+++ RevenuePlanner/Controllers/PlanController.cs
@@ -245,7 +245,14 @@ namespace RevenuePlanner.Controllers
                     objPlanModel.Year = objplan.Year;
                     objPlanModel.GoalType = GoalTypeList.Where(a => a.Value == objplan.GoalType).Select(a => a.Value).FirstOrDefault();
                     //objPlanModel.GoalValue = Convert.ToString(objplan.GoalValue);                  
-                    objPlanModel.GoalValue = objCurrency.GetValueByExchangeRate(double.Parse(Convert.ToString(objplan.GoalValue)), PlanExchangeRate).ToString();
+                    if (Convert.ToString(objPlanModel.GoalType).ToUpper() == Enums.PlanGoalType.Revenue.ToString().ToUpper())
+                    {
+                        objPlanModel.GoalValue = objCurrency.GetValueByExchangeRate(double.Parse(Convert.ToString(objplan.GoalValue)), PlanExchangeRate).ToString();
+                    }
+                    else
+                    {
+                        objPlanModel.GoalValue = Convert.ToString(objplan.GoalValue);
+                    }
                     objPlanModel.AllocatedBy = objplan.AllocatedBy;
                     //objPlanModel.Budget = objplan.Budget;
                     objPlanModel.Budget = objCurrency.GetValueByExchangeRate(double.Parse(Convert.ToString(objplan.Budget)), PlanExchangeRate);
