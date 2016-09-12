e632aee702ec302906d8917063a2653ecfc25f3d
Devanshi <devanshi.gandhi@indusa.com>
2016-09-12 16:59:08 +0530

    resolved observation regarding pl ticket #2588 for order of attributes

--- Database Scripts/09-2016_Migration_Script_MRP.sql
+++ Database Scripts/09-2016_Migration_Script_MRP.sql
@@ -152,15 +152,30 @@ AS
 BEGIN
 SET NOCOUNT ON;
 
-SELECT distinct CustomField.*,ISnull(CustomFieldDependency.ParentCustomFieldId,0) as ParentId 
+Declare @CustomfieldType int
+set @CustomfieldType=(select CustomFieldTypeId from CustomFieldType where Name='DropDownList')
+
+SELECT distinct CustomField.CustomFieldId,CustomField.Name,CustomField.IsRequired,case when  CustomField.EntityType='LineItem' then 'Line Item' else CustomField.EntityType end as EntityType,ISnull(CustomFieldDependency.ParentCustomFieldId,0) as ParentId ,CustomFieldType.Name as CustomFieldType,
+case when  CustomField.EntityType='Campaign' then 1
+when  CustomField.EntityType='Program' then 2
+when  CustomField.EntityType='Tactic' then 3
+when  CustomField.EntityType='LineItem' then 4
+end as entityorder
 FROM CustomField (NOLOCK) 
+inner join CustomFieldType on CustomField.CustomFieldTypeId=CustomFieldType.CustomFieldTypeId
 LEFT join CustomFieldDependency on
-CustomField.CustomFieldId= CustomFieldDependency.ChildCustomFieldId WHERE 
-CustomField.IsDeleted=0
-AND ClientId= CASE WHEN @ClientId IS NULL THEN ClientId ELSE @ClientId END
-order by CustomField.Name
+CustomField.CustomFieldId= CustomFieldDependency.ChildCustomFieldId
+Left join CustomFieldOption on CustomField.CustomFieldId = CustomFieldOption.CustomFieldId 
+ WHERE 
+CustomField.IsDeleted=0 and (CustomField.CustomFieldTypeId <> @CustomfieldType or (CustomFieldOptionId IS NOT NULL AND CustomField.CustomFieldTypeId=@CustomfieldType ))
+and ClientId= CASE WHEN @ClientId IS NULL THEN ClientId ELSE @ClientId END and CustomField.EntityType in('Campaign','Program','Tactic','LineItem')
+group by CustomField.CustomFieldId,CustomField.Name,CustomField.IsRequired,CustomField.EntityType,CustomFieldDependency.ParentCustomFieldId,CustomFieldType.Name
+order by entityorder
 
 END
+
+--select * from CustomFieldType where CustomFieldId=29
+-- EXEC sp_GetCustomFieldList '464eb808-ad1f-4481-9365-6aada15023bd'
 GO
 /****** Object:  StoredProcedure [dbo].[spViewByDropDownList]    Script Date: 09/08/2016 12:46:47 PM ******/
 SET ANSI_NULLS ON
