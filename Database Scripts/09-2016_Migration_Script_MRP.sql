1e13c0aaa719101cd212bf91a57af790cb02b3bc
Viral Kadiya <viral.kadiya@indusa.com>
2016-09-13 13:30:51 +0530

    I have committed SQL functions for the following subject. - Create Hierarchy list based on plan Ids - Return list of data with required filters

--- Database Scripts/09-2016_Migration_Script_MRP.sql
+++ Database Scripts/09-2016_Migration_Script_MRP.sql
@@ -299,6 +299,168 @@ END
 GO
 /* End - Added by Arpita Soni for Ticket #2612 on 09/08/2016 */
 
+/* Start- Added by Viral for Ticket #2571 on 09/13/2016 */
+
+/****** Object:  UserDefinedFunction [dbo].[fnGetEntitieHirarchyByPlanId]    Script Date: 09/13/2016 12:55:37 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[fnGetEntitieHirarchyByPlanId]') AND type in (N'FN', N'IF', N'TF', N'FS', N'FT'))
+BEGIN
+execute dbo.sp_executesql @statement = N'--This function will return all the enties with hirarchy
+--Multiple plan ids can be passed saperated by comma
+--If we pass null then it will retuen all plans hirarchy data
+CREATE FUNCTION [dbo].[fnGetEntitieHirarchyByPlanId] ( @PlanIds NVARCHAR(MAX))
+RETURNS @Entities TABLE (
+			UniqueId		NVARCHAR(30), 
+			EntityId		BIGINT,
+			EntityTitle		NVARCHAR(1000),
+			ParentEntityId	BIGINT, 
+			ParentUniqueId	NVARCHAR(30),
+			EntityType		NVARCHAR(15), 
+			ColorCode		NVARCHAR(7),
+			[Status]		NVARCHAR(15), 
+			StartDate		DATETIME, 
+			EndDate			DATETIME, 
+			CreatedBy		UNIQUEIDENTIFIER
+		)
+AS
+BEGIN
+
+	;WITH FilteredPlan AS(
+		SELECT ''Plan'' EntityType,''P_'' + CAST(P.PlanId AS NVARCHAR(10)) UniqueId,P.PlanId EntityId, P.Title EntityTitle,NULL ParentEntityId,NULL ParentUniqueId, P.Status, NULL StartDate, NULL EndDate,P.CreatedBy FROM [Plan] P 
+			--INNER JOIN Model M ON M.ModelId = P.ModelId AND M.ClientId = @ClientId
+		WHERE P.IsDeleted = 0 
+			AND (
+					@PlanIds IS NULL 
+					OR P.PlanId IN (SELECT DISTINCT dimension FROM dbo.fnSplitString(@PlanIds,'',''))
+				)
+	),
+	Campaigns AS (
+		SELECT ''Campaign'' EntityType,''P_C_'' + CAST(C.PlanCampaignId AS NVARCHAR(10)) UniqueId,C.PlanCampaignId EntityId, C.Title EntityTitle, P.EntityId ParentEntityId,P.UniqueId ParentUniqueId, C.Status, C.StartDate StartDate, C.EndDate EndDate,C.CreatedBy FROM Plan_Campaign C
+			INNER JOIN FilteredPlan P ON P.EntityId = C.PlanId 
+		WHERE C.IsDeleted = 0 
+	),
+	Programs AS (
+		SELECT ''Program'' EntityType,''P_C_P_'' + CAST(P.PlanProgramId AS NVARCHAR(10)) UniqueId,P.PlanProgramId EntityId, P.Title EntityTitle, C.EntityId ParentEntityId,C.UniqueId ParentUniqueId, P.Status, P.StartDate StartDate, P.EndDate EndDate,P.CreatedBy FROM Plan_Campaign_Program P
+			INNER JOIN Campaigns C ON C.EntityId = P.PlanCampaignId
+		WHERE P.IsDeleted = 0 
+	),
+	Tactics AS (
+		SELECT ''Tactic'' EntityType,''P_C_P_T_'' + CAST(T.PlanTacticId AS NVARCHAR(10)) UniqueId,T.PlanTacticId EntityId, T.Title EntityTitle, P.EntityId ParentEntityId,P.UniqueId ParentUniqueId, T.Status, T.StartDate StartDate, T.EndDate EndDate,T.CreatedBy FROM Plan_Campaign_Program_Tactic T
+			INNER JOIN Programs P ON P.EntityId = T.PlanProgramId
+		WHERE T.IsDeleted = 0 
+	),
+	LineItems AS (
+		SELECT ''LineItem'' EntityType,''P_C_P_T_L_'' + CAST(L.PlanLineItemId AS NVARCHAR(10)) UniqueId,L.PlanLineItemId EntityId, L.Title EntityTitle, T.EntityId ParentEntityId,T.UniqueId ParentUniqueId, NULL Status, L.StartDate StartDate, L.EndDate EndDate,L.CreatedBy FROM Plan_Campaign_Program_Tactic_LineItem L
+			INNER JOIN Tactics T ON T.EntityId = L.PlanTacticId
+		WHERE L.IsDeleted = 0 
+	),
+	AllEntities AS (    
+		SELECT * FROM FilteredPlan UNION ALL
+		SELECT * FROM Campaigns UNION ALL
+		SELECT * FROM Programs UNION ALL
+		SELECT * FROM Tactics UNION ALL
+		SELECT * FROM LineItems
+	)
+	INSERT INTO @Entities (UniqueId, EntityId,EntityTitle, ParentEntityId,ParentUniqueId,EntityType, ColorCode,Status,StartDate,EndDate,CreatedBy)
+	SELECT E.UniqueId, E.EntityId,E.EntityTitle, E.ParentEntityId,E.ParentUniqueId,E.EntityType, C.ColorCode,E.Status,E.StartDate,E.EndDate,E.CreatedBy FROM AllEntities E
+	LEFT JOIN EntityTypeColor C ON C.EntityType = E.EntityType
+
+	RETURN
+END
+
+
+' 
+END
+
+GO
+
+/* End- Added by Viral for Ticket #2571 on 09/13/2016 */
+
+/* Start- Added by Viral for Ticket #2571 on 09/13/2016 */
+
+/****** Object:  UserDefinedFunction [dbo].[fnGetFilterEntityHierarchy]    Script Date: 09/13/2016 12:55:37 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[fnGetFilterEntityHierarchy]') AND type in (N'FN', N'IF', N'TF', N'FS', N'FT'))
+BEGIN
+execute dbo.sp_executesql @statement = N'-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE FUNCTION [dbo].[fnGetFilterEntityHierarchy]
+(
+	@planIds varchar(max)='''',
+	--@customfields varchar(max)=''71_104,71_105'',
+	@ownerIds nvarchar(max)='''',
+	@tactictypeIds varchar(max)='''',
+	@statusIds varchar(max)=''''
+)
+
+--select * from fnGetFilterEntityHierarchy(''20220'','''',''56_null'',''41F64F4B-531E-4CAA-8F5F-328E36D9B202'',''31104'',''Created'')
+RETURNS @Entities TABLE (
+			UniqueId		NVARCHAR(30), 
+			EntityId		BIGINT,
+			EntityTitle		NVARCHAR(1000),
+			ParentEntityId	BIGINT, 
+			ParentUniqueId	NVARCHAR(30),
+			EntityType		NVARCHAR(15), 
+			ColorCode		NVARCHAR(7),
+			[Status]		NVARCHAR(15), 
+			StartDate		DATETIME, 
+			EndDate			DATETIME, 
+			CreatedBy		UNIQUEIDENTIFIER
+		)
+AS
+BEGIN
+
+
+Declare @entTactic varchar(8)=''Tactic''
+Declare @entLineItem varchar(10)=''LineItem''
+
+	-- Fill the table variable with the rows for your result set
+	
+
+	;WITH FilteredEnt AS(
+Select * from fnGetEntitieHirarchyByPlanId(@planIds)
+)
+,tac as (
+	Select distinct ent.* 
+	FROM FilteredEnt as ent
+	Join [Plan_Campaign_Program_Tactic] as tac on ent.EntityId = tac.PlanTacticId and ent.EntityType=@entTactic AND tac.[Status] IN (select val from comma_split(@statusIds,'','')) and  tac.[CreatedBy] IN (select case when val = '''' then null else Convert(uniqueidentifier,val) end from comma_split(@ownerIds,'',''))
+	Join [TacticType] as typ on tac.TacticTypeId = typ.TacticTypeId and typ.IsDeleted=''0'' and typ.[TacticTypeId] IN (select val from comma_split(@tactictypeIds,'',''))
+	where ent.EntityType = @entTactic
+)
+,line as (
+	SELECT ent.* 
+	FROM FilteredEnt as ent
+	JOIN tac on ent.ParentEntityId = tac.EntityId and ent.EntityType=@entLineItem
+
+)
+
+INSERT INTO @Entities
+select * from FilteredEnt where EntityType not in (''Tactic'',''LineItem'')
+union all
+SELECT * FROM tac 
+union all
+select * from line
+
+RETURN
+
+END
+' 
+END
+
+GO
+
+
+/* End- Added by Viral for Ticket #2571 on 09/13/2016 */
+
 
 -- ===========================Please put your script above this script=============================
 -- Description :Ensure versioning table exists & Update versioning table with script version
