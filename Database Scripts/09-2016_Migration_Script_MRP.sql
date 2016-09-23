e5c789030e7e3397e2d2edd57658b6e103cba9e0
Dhvani Raval <dhvani.raval@indusa.com>
2016-09-23 18:34:11 +0530

    Commiting this Store procedure for ticket - 2534

--- Database Scripts/09-2016_Migration_Script_MRP.sql
+++ Database Scripts/09-2016_Migration_Script_MRP.sql
@@ -3437,6 +3437,183 @@ END
 
 GO
 
+-----------------------* SP sp_Send_Mail_For_Alerts *-----------------------
+
+IF EXISTS( SELECT  * FROM sys.objects WHERE  object_id = OBJECT_ID(N'sp_Send_Mail_For_Alerts') AND type IN ( N'P', N'PC' ) )
+BEGIN
+	DROP PROCEDURE dbo.sp_Send_Mail_For_Alerts
+END
+GO
+
+
+CREATE PROCEDURE [dbo].[sp_Send_Mail_For_Alerts]
+        @from Nvarchar(500),
+		@pwd Nvarchar(30),
+		@Url Nvarchar(max),
+		@SMTPPort Nvarchar(10),
+		@SMTPServerAddress Nvarchar(100)	
+AS 
+BEGIN
+
+  DECLARE @imsg int
+  DECLARE @hr int
+  DECLARE @source varchar(255)
+  DECLARE @description varchar(500)
+  DECLARE @bodytype varchar(10)
+  DECLARE @to varchar(500) 
+  DECLARE @Comparision varchar(500)
+  DECLARE @body varchar(max) 
+  DECLARE @subject varchar(max) 
+  DECLARE @output_desc varchar(1000)
+  DECLARE @Result varchar(1000)
+  DECLARE @UrlString varchar(max)
+  DECLARE @SubjectComparision varchar(500)
+  DECLARE @Query Nvarchar(max)
+   
+  SET @bodytype = 'htmlbody'
+  
+  BEGIN
+  DECLARE @AlertId int, @AlertDescription nvarchar(max), @Email  nvarchar(255),@Indicator nvarchar(50), @IndicatorComparision nvarchar(10),@IndicatorGoal int, @EntityTitle nvarchar(500),@DisplayDate DateTime, @ActualGoal float, @CurrentGoal float,@PlanName nvarchar(255),@Entity nvarchar(255),@PlanId int,@EntityId int
+  			
+  DECLARE Cur_Mail CURSOR FOR	
+               SELECT  al.AlertId, al.Description, ar.UserEmail, 
+				CASE WHEN ar.Indicator = 'PLANNEDCOST' 
+					THEN 'PLANNED COST'
+					when ar.Indicator = 'REVENUE' 
+					THEN 'REVENUE'
+					ELSE Stage.Title END AS
+					 Indicator, ar.IndicatorComparision,ar.IndicatorGoal, vw.EntityTitle,al.DisplayDate,al.ActualGoal,al.CurrentGoal, vw.PlanTitle,vw.Entity, vw.PlanId,vw.EntityId
+			   FROM  [dbo].Alerts al
+			   INNER JOIN dbo.[Alert_Rules] AS ar ON ar.RuleId = al.RuleId  and ar.UserId = al.UserId
+			   left JOIN dbo.Stage ON Stage.Code = ar.Indicator  and ar.ClientId = Stage.ClientId
+			   left JOIN dbo.vClientWise_EntityList as vw on  vw.EntityId = ar.EntityId 
+			   WHERE al.DisplayDate <= GETDATE() and  (ISNULL(ar.UserEmail,'') != '' ) and (al.IsEmailSent Is null or  al.IsEmailSent <> 'Success') and ar.UserEmail Is Not Null
+   
+    
+  OPEN Cur_Mail
+  
+        FETCH NEXT FROM Cur_Mail INTO  @AlertId, @AlertDescription, @Email, @Indicator, @IndicatorComparision, @IndicatorGoal, @EntityTitle, @DisplayDate,@ActualGoal,@CurrentGoal,@PlanName,@Entity,@PlanId,@EntityId
+        
+		
+        WHILE @@FETCH_STATUS=0
+        BEGIN
+
+
+		Begin
+		--print  @Email
+        EXEC @hr = sp_oacreate 'cdo.message', @imsg out
+        
+        --SendUsing Specifies Whether to send using port (2) or using pickup directory (1)
+        EXEC @hr = sp_oasetproperty @imsg, 'configuration.fields("http://schemas.microsoft.com/cdo/configuration/sendusing").value','2'
+        
+        --SMTP Server
+        EXEC @hr = sp_oasetproperty @imsg, 'configuration.fields("http://schemas.microsoft.com/cdo/configuration/smtpserver").value',@SMTPServerAddress 
+        
+		--PORT 
+        EXEC @hr = sp_oasetproperty @imsg, 'configuration.fields("http://schemas.microsoft.com/cdo/configuration/smtpserverport").value',@SMTPPort
+
+        --UserName
+        EXEC @hr = sp_oasetproperty @imsg, 'configuration.fields("http://schemas.microsoft.com/cdo/configuration/sendusername").value',@from
+        
+        --Password
+        EXEC @hr = sp_oasetproperty @imsg, 'configuration.fields("http://schemas.microsoft.com/cdo/configuration/sendpassword").value',@pwd
+  
+        --UseSSL
+        EXEC @hr = sp_oasetproperty @imsg, 'configuration.fields("http://schemas.microsoft.com/cdo/configuration/smtpusessl").value','True' 
+        			       
+        --Requires Aunthentication None(0) / Basic(1)
+        EXEC @hr = sp_oasetproperty @imsg, 'configuration.fields("http://schemas.microsoft.com/cdo/configuration/smtpauthenticate").value','1' 	
+        
+	END
+      
+        IF @IndicatorComparision = 'GT'
+		BEGIN
+              set  @Comparision = '<b> greater than </b>'
+			  set @SubjectComparision = 'above goal'
+			  END
+        ELSE 	
+		BEGIN
+		      IF @IndicatorComparision = 'LT'
+			  	 BEGIN
+                  set @Comparision = '<b>  less than </b>' 
+				  set @SubjectComparision = 'below goal'
+				 END
+              ELSE 
+			     BEGIN
+                  set @Comparision = '<b> equal to </b>' 
+				  set @SubjectComparision = 'equal goal'
+				END
+		END
+		
+        Set @subject = @EntityTitle + ' is performing ' + @SubjectComparision 
+
+		SET @body = @EntityTitle + '''s <b>' + @Indicator +' </b> is '+ @Comparision + ' ' + CONVERT(nvarchar(50),@IndicatorGoal) +'% of the goal as of <b>'+ CONVERT(VARCHAR(11),@DisplayDate,106)  + '</b><br><br>Item : ' +  @EntityTitle + '<br>Plan Name : '+ @PlanName 
+	    
+		IF @ActualGoal is not null
+		SET @body = @body +  '<br>Goal : '+ cast(Format(@ActualGoal, '##,##0') as varchar) 
+
+		IF @CurrentGoal is not null
+		SET @body = @body + '<br>Current : ' + cast(Format(@CurrentGoal, '##,##0') as varchar)
+		 
+		 IF @Entity <> 'Plan' 
+		 BEGIN
+		 SET @UrlString = @Url +'/home?currentPlanId='+convert(nvarchar(max), @planId)+'&plan'+@Entity+'Id='+convert(nvarchar(max),@EntityId)+'&activeMenu=Plan'
+		 END
+		 ELSE
+		 BEGIN
+		 SET @UrlString = @Url +'/home?currentPlanId='+convert(nvarchar(max), @planId)+'&activeMenu=Plan'
+		 End
+
+		set @body = @body + '<br><br><html><body> URL : <a href=' + @UrlString +'>'+@UrlString+'</a></body></html>' 
+
+        
+        EXEC @hr = sp_oamethod @imsg, 'configuration.fields.update', null
+        EXEC @hr = sp_oasetproperty @imsg, 'to', @Email
+        EXEC @hr = sp_oasetproperty @imsg, 'from', @from
+        EXEC @hr = sp_oasetproperty @imsg, 'subject', @subject
+        EXEC @hr = sp_oasetproperty @imsg, @bodytype, @body
+        EXEC @hr = sp_oamethod @imsg, 'send',null        
+		
+        -- sample error handling.	
+        IF @hr <> 0   
+        	BEGIN
+			    EXEC @hr = sp_oageterrorinfo null, out, @description out
+			    IF @hr = 0
+        		BEGIN					
+        		
+					update Alerts
+            		set IsEmailSent =  @description  --'Not Sent'
+        			where AlertId = @AlertId
+				
+				End
+				ELSE
+				BEGIN
+        		    update Alerts
+            		set IsEmailSent = 'FAIL'
+        			where AlertId = @AlertId
+				END
+			END
+        ELSE
+        	BEGIN
+        		update  Alerts
+        		set IsEmailSent = 'Success'
+        		where AlertId = @AlertId
+        	END
+
+             
+        EXEC @hr = sp_oadestroy @imsg
+        
+        FETCH NEXT FROM Cur_Mail INTO  @AlertId, @AlertDescription, @Email, @Indicator, @IndicatorComparision, @IndicatorGoal, @EntityTitle, @DisplayDate, @ActualGoal, @CurrentGoal, @PlanName,@Entity,@PlanId,@EntityId
+  
+		END
+  CLOSE Cur_Mail
+  DEALLOCATE Cur_Mail
+  END
+  END
+  Go
+
+
+
 
 -- ===========================Please put your script above this script=============================
 -- Description :Ensure versioning table exists & Update versioning table with script version
