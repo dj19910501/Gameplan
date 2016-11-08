21712f725103d1fd6b0656b2c456700c5fc81db9
Nishant Sheth <nishant.sheth@indusa.com>
2016-11-08 16:11:03 +0530

    I have made changes regarding implement WITH (NOLOCK) for concurrent issues.

--- Database Scripts/09-2016_Migration_Script_MRP.sql
+++ Database Scripts/09-2016_Migration_Script_MRP.sql
@@ -820,23 +820,18 @@ GO
 -- Merge Filter EntityHierarchy and EntityHierarchy function into single function
 /****** Object:  UserDefinedFunction [dbo].[fnGetFilterEntityHierarchy]    Script Date: 11/04/2016 12:21:55 PM ******/
 IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[fnGetFilterEntityHierarchy]') AND type in (N'FN', N'IF', N'TF', N'FS', N'FT'))
-DROP FUNCTION [dbo].[fnGetFilterEntityHierarchy]
+	DROP FUNCTION [dbo].[fnGetFilterEntityHierarchy]
 GO
 /****** Object:  UserDefinedFunction [dbo].[fnGetFilterEntityHierarchy]    Script Date: 11/04/2016 12:21:55 PM ******/
 SET ANSI_NULLS ON
 GO
-SET QUOTED_IDENTIFIER ON
-GO
-IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[fnGetFilterEntityHierarchy]') AND type in (N'FN', N'IF', N'TF', N'FS', N'FT'))
-BEGIN
-execute dbo.sp_executesql @statement = N'
 CREATE FUNCTION [dbo].[fnGetFilterEntityHierarchy]
 (
-	@planIds varchar(max)='''',
-	@ownerIds nvarchar(max)='''',
-	@tactictypeIds varchar(max)='''',
-	@statusIds varchar(max)='''',
-	@TimeFrame varchar(20)='''',
+	@planIds varchar(max)='',
+	@ownerIds nvarchar(max)='',
+	@tactictypeIds varchar(max)='',
+	@statusIds varchar(max)='',
+	@TimeFrame varchar(20)='',
 	@isGrid bit=0
 )
 
@@ -880,16 +875,16 @@ BEGIN
 	DECLARE @EndDate DateTime   --End date perameter to filter entities e.g. campaign,program and tactic
 
 	--Set first year with first start date of year and last year with last date of year
-	IF (@TimeFrame IS NOT NULL AND @TimeFrame <>'''')
+	IF (@TimeFrame IS NOT NULL AND @TimeFrame <>'')
 		BEGIN
 			INSERT INTO @TimeFrameDatesAndYear
-			SELECT Item as PlanYear from dbo.SplitString(@TimeFrame,''-'')--split timeframe parameter e.g. 2015-2016
+			SELECT Item as PlanYear from dbo.SplitString(@TimeFrame,'-')--split timeframe parameter e.g. 2015-2016
 			
 			--Set Minimum & Maximumyear
 			SELECT @MinYear=CONVERT(VARCHAR,( MIN(CONVERT(INT,PlanYear)))),@MaxYear=CONVERT(VARCHAR,( MAX(CONVERT(INT,PlanYear)))) FROM @TimeFrameDatesAndYear 
 	
-			SET @StartDate= CONVERT(DATETIME,@MinYear+''-01-01 00:00:00'') --Set first date of minimum year
-			SET @EndDate= CONVERT(DATETIME,@MaxYear+''-12-31 00:00:00'')   --Set Last date of maximum year
+			SET @StartDate= CONVERT(DATETIME,@MinYear+'-01-01 00:00:00') --Set first date of minimum year
+			SET @EndDate= CONVERT(DATETIME,@MaxYear+'-12-31 00:00:00')   --Set Last date of maximum year
 
 		END
 	
@@ -898,21 +893,21 @@ BEGIN
 	BEGIN
 		INSERT INTO @Entities(UniqueId,EntityId,EntityTitle,EntityType,[Status],CreatedBy,AltId
 										,TaskId,PlanId,ModelId)
-		SELECT ''P_'' + CAST(P.PlanId AS NVARCHAR(10)) UniqueId
+		SELECT 'P_' + CAST(P.PlanId AS NVARCHAR(10)) UniqueId
 				,P.PlanId EntityId
 				, P.Title EntityTitle
-				,''Plan'' EntityType
+				,'Plan' EntityType
 				, P.Status
 				, P.CreatedBy 
 				,CAST(P.PlanId AS NVARCHAR(50)) AS AltId
-				,''L''+CAST(P.PlanId AS NVARCHAR(50)) AS TaskId
+				,'L'+CAST(P.PlanId AS NVARCHAR(50)) AS TaskId
 				,P.PlanId
 				,P.ModelId
-		FROM [Plan] P 
+		FROM [Plan] P WITH(NOLOCK)
 		WHERE P.IsDeleted = 0 
 				AND (
 						@PlanIds IS NULL 
-						OR P.PlanId IN (SELECT DISTINCT dimension FROM dbo.fnSplitString(@PlanIds,'',''))
+						OR P.PlanId IN (SELECT DISTINCT dimension FROM dbo.fnSplitString(@PlanIds,','))
 					) 
 	END
 
@@ -920,24 +915,24 @@ BEGIN
 	BEGIN
 		INSERT INTO @Entities(UniqueId,EntityId,EntityTitle,ParentEntityId,ParentUniqueId,EntityType,[Status],StartDate,EndDate,CreatedBy,AltId
 										,TaskId,ParentTaskId,PlanId,ModelId)
-		SELECT ''P_C_'' + CAST(C.PlanCampaignId AS NVARCHAR(10)) UniqueId
+		SELECT 'P_C_' + CAST(C.PlanCampaignId AS NVARCHAR(10)) UniqueId
 				,C.PlanCampaignId EntityId
 				, C.Title EntityTitle
 				, P.EntityId ParentEntityId
 				,P.UniqueId ParentUniqueId
-				,''Campaign'' EntityType
+				,'Campaign' EntityType
 				, C.Status
 				, C.StartDate StartDate
 				, C.EndDate EndDate
 				,C.CreatedBy 
-			,CAST(C.PlanId AS NVARCHAR(500))+''_''+CAST(C.PlanCampaignId AS NVARCHAR(50)) AS AltId
-			,CAST(P.TaskId AS NVARCHAR(500))+''_C''+CAST(C.PlanCampaignId AS NVARCHAR(50)) AS TaskId
-			,''L''+CAST(C.PlanId  AS NVARCHAR(500)) AS ParentTaskId
+			,CAST(C.PlanId AS NVARCHAR(500))+'_'+CAST(C.PlanCampaignId AS NVARCHAR(50)) AS AltId
+			,CAST(P.TaskId AS NVARCHAR(500))+'_C'+CAST(C.PlanCampaignId AS NVARCHAR(50)) AS TaskId
+			,'L'+CAST(C.PlanId  AS NVARCHAR(500)) AS ParentTaskId
 			,P.PlanId
 			,P.ModelId
 			
-			FROM Plan_Campaign C
-			INNER JOIN @Entities P ON P.EntityId = C.PlanId and P.EntityType=''Plan''
+			FROM Plan_Campaign C WITH(NOLOCK)
+			INNER JOIN @Entities P ON P.EntityId = C.PlanId and P.EntityType='Plan'
 			WHERE C.IsDeleted = 0 AND (@isGrid=1 OR (C.StartDate>=@StartDate AND C.StartDate<=@EndDate) OR (C.EndDate>=@StartDate AND C.EndDate<=@EndDate))
 	END
 
@@ -946,16 +941,16 @@ BEGIN
 		INSERT INTO @Entities(UniqueId,EntityId,EntityTitle,ParentEntityId,ParentUniqueId,EntityType,[Status],StartDate,EndDate,CreatedBy,AltId
 										,TaskId,ParentTaskId,PlanId,ModelId)
 
-		SELECT ''P_C_P_'' + CAST(P.PlanProgramId AS NVARCHAR(10)) UniqueId
-			,P.PlanProgramId EntityId, P.Title EntityTitle, C.EntityId ParentEntityId,C.UniqueId ParentUniqueId,''Program'' EntityType, P.Status, P.StartDate StartDate, P.EndDate EndDate,P.CreatedBy 
-			,CAST(P.PlanCampaignId AS NVARCHAR(500))+''_''+CAST(P.PlanProgramId AS NVARCHAR(50)) As AltId
-			,CAST(C.TaskId AS NVARCHAR(500))+''_P''+CAST(P.PlanProgramId AS NVARCHAR(50)) As TaskId
-			,CAST(C.ParentTaskId AS NVARCHAR(500))+''_C''+CAST(P.PlanCampaignId AS NVARCHAR(50)) As ParentTaskId
+		SELECT 'P_C_P_' + CAST(P.PlanProgramId AS NVARCHAR(10)) UniqueId
+			,P.PlanProgramId EntityId, P.Title EntityTitle, C.EntityId ParentEntityId,C.UniqueId ParentUniqueId,'Program' EntityType, P.Status, P.StartDate StartDate, P.EndDate EndDate,P.CreatedBy 
+			,CAST(P.PlanCampaignId AS NVARCHAR(500))+'_'+CAST(P.PlanProgramId AS NVARCHAR(50)) As AltId
+			,CAST(C.TaskId AS NVARCHAR(500))+'_P'+CAST(P.PlanProgramId AS NVARCHAR(50)) As TaskId
+			,CAST(C.ParentTaskId AS NVARCHAR(500))+'_C'+CAST(P.PlanCampaignId AS NVARCHAR(50)) As ParentTaskId
 			,C.PlanId
 			,C.ModelId
 		
-			FROM Plan_Campaign_Program P
-			INNER JOIN @Entities C ON C.EntityId = P.PlanCampaignId and C.EntityType=''Campaign''
+			FROM Plan_Campaign_Program P WITH(NOLOCK)
+			INNER JOIN @Entities C ON C.EntityId = P.PlanCampaignId and C.EntityType='Campaign'
 			WHERE P.IsDeleted = 0 AND (@isGrid=1 OR (P.StartDate>=@StartDate AND P.StartDate<=@EndDate) OR (P.EndDate>=@StartDate AND P.EndDate<=@EndDate))
 	END
 
@@ -964,34 +959,34 @@ BEGIN
 		INSERT INTO @Entities(UniqueId,EntityId,EntityTitle,ParentEntityId,ParentUniqueId,EntityType,[Status],StartDate,EndDate,CreatedBy,AltId
 										,TaskId,ParentTaskId,PlanId,ModelId,ROIPackageIds)
 
-		SELECT ''P_C_P_T_'' + CAST(T.PlanTacticId AS NVARCHAR(10)) UniqueId,T.PlanTacticId EntityId, T.Title EntityTitle, P.EntityId ParentEntityId,P.UniqueId ParentUniqueId,''Tactic'' EntityType, T.Status, T.StartDate StartDate, T.EndDate EndDate,T.CreatedBy 
-			,CAST(T.PlanProgramId AS NVARCHAR(500))+''_''+CAST(T.PlanTacticId AS NVARCHAR(50)) As AltId
-			,CAST(P.TaskId AS NVARCHAR(500))+''_T''+CAST(T.PlanTacticId AS NVARCHAR(50)) As TaskId
-			,CAST(P.ParentTaskId AS NVARCHAR(500))+''_P''+CAST(T.PlanProgramId AS NVARCHAR(50)) As ParentTaskId
+		SELECT 'P_C_P_T_' + CAST(T.PlanTacticId AS NVARCHAR(10)) UniqueId,T.PlanTacticId EntityId, T.Title EntityTitle, P.EntityId ParentEntityId,P.UniqueId ParentUniqueId,'Tactic' EntityType, T.Status, T.StartDate StartDate, T.EndDate EndDate,T.CreatedBy 
+			,CAST(T.PlanProgramId AS NVARCHAR(500))+'_'+CAST(T.PlanTacticId AS NVARCHAR(50)) As AltId
+			,CAST(P.TaskId AS NVARCHAR(500))+'_T'+CAST(T.PlanTacticId AS NVARCHAR(50)) As TaskId
+			,CAST(P.ParentTaskId AS NVARCHAR(500))+'_P'+CAST(T.PlanProgramId AS NVARCHAR(50)) As ParentTaskId
 			,P.PlanId
 			,P.ModelId
 			
 			,R.PackageIds as ROIPackageIds
-			FROM Plan_Campaign_Program_Tactic T
-			INNER JOIN @Entities P ON P.EntityId = T.PlanProgramId and P.EntityType=''Program''
-			INNER JOIN [TacticType] as typ on T.TacticTypeId = typ.TacticTypeId and typ.IsDeleted=''0'' and 
-			(@tactictypeIds = ''All'' OR typ.[TacticTypeId] IN (select val from comma_split(@tactictypeIds,'','')))
+			FROM Plan_Campaign_Program_Tactic T WITH(NOLOCK)
+			INNER JOIN @Entities P ON P.EntityId = T.PlanProgramId and P.EntityType='Program'
+			INNER JOIN [TacticType]  as typ WITH(NOLOCK) on T.TacticTypeId = typ.TacticTypeId and typ.IsDeleted='0' and 
+			(@tactictypeIds = 'All' OR typ.[TacticTypeId] IN (select val from comma_split(@tactictypeIds,',')))
 
 			LEFT JOIN (SELECT AnchorTacticID,PackageIds=
-					STUFF((SELECT '', '' + Cast(PlanTacticId as varchar)
+					STUFF((SELECT ', ' + Cast(PlanTacticId as varchar)
 					       FROM ROI_PackageDetail b 
 					       WHERE b.AnchorTacticID = a.AnchorTacticID 
-					      FOR XML PATH('''')), 1, 2, '''')
+					      FOR XML PATH('')), 1, 2, '')
 					FROM @Entities as P1
-					JOIN Plan_Campaign_Program_Tactic T2 on P1.EntityId = T2.PlanProgramId and P1.EntityType=''Program''
-					JOIN ROI_PackageDetail as a on T2.PlanTacticId = a.AnchorTacticID
+					JOIN Plan_Campaign_Program_Tactic T2 WITH(NOLOCK) on P1.EntityId = T2.PlanProgramId and P1.EntityType='Program'
+					JOIN ROI_PackageDetail as a WITH(NOLOCK)on T2.PlanTacticId = a.AnchorTacticID
 					GROUP BY a.AnchorTacticID
 					
 				) as R on T.PlanTacticId = R.AnchorTacticId
 
 			WHERE T.IsDeleted = 0 AND (@isGrid=1 OR (T.StartDate>=@StartDate AND T.StartDate<=@EndDate) OR (T.EndDate>=@StartDate AND T.EndDate<=@EndDate))
-					AND T.[Status] IN (select val from comma_split(@statusIds,'','')) and  
-					(@ownerIds = ''All'' OR T.[CreatedBy] IN (select case when val = '''' then null else Convert(int,val) end from comma_split(@ownerIds,'','')))
+					AND T.[Status] IN (select val from comma_split(@statusIds,',')) and  
+					(@ownerIds = 'All' OR T.[CreatedBy] IN (select case when val = '' then null else Convert(int,val) end from comma_split(@ownerIds,',')))
 	END
 	
 	
@@ -1000,15 +995,15 @@ BEGIN
 	BEGIN
 		INSERT INTO @Entities(UniqueId,EntityId,EntityTitle,ParentEntityId,ParentUniqueId,EntityType,StartDate,EndDate,CreatedBy,AltId
 										,TaskId,ParentTaskId,PlanId,ModelId)
-		SELECT ''P_C_P_T_L_'' + CAST(L.PlanLineItemId AS NVARCHAR(10)) UniqueId,L.PlanLineItemId EntityId, L.Title EntityTitle, T.EntityId ParentEntityId,T.UniqueId ParentUniqueId,''LineItem'' EntityType, L.StartDate StartDate, L.EndDate EndDate,L.CreatedBy 
-			,CAST(L.PlanTacticId AS NVARCHAR(500))+''_''+CAST(L.PlanLineItemId AS NVARCHAR(50)) As AltId
-			,CAST(T.TaskId AS NVARCHAR(500))+''_X''+CAST(L.PlanLineItemId AS NVARCHAR(50)) As TaskId
-			,CAST(T.ParentTaskId AS NVARCHAR(500))+''_T''+CAST(L.PlanTacticId AS NVARCHAR(50)) As ParentTaskId
+		SELECT 'P_C_P_T_L_' + CAST(L.PlanLineItemId AS NVARCHAR(10)) UniqueId,L.PlanLineItemId EntityId, L.Title EntityTitle, T.EntityId ParentEntityId,T.UniqueId ParentUniqueId,'LineItem' EntityType, L.StartDate StartDate, L.EndDate EndDate,L.CreatedBy 
+			,CAST(L.PlanTacticId AS NVARCHAR(500))+'_'+CAST(L.PlanLineItemId AS NVARCHAR(50)) As AltId
+			,CAST(T.TaskId AS NVARCHAR(500))+'_X'+CAST(L.PlanLineItemId AS NVARCHAR(50)) As TaskId
+			,CAST(T.ParentTaskId AS NVARCHAR(500))+'_T'+CAST(L.PlanTacticId AS NVARCHAR(50)) As ParentTaskId
 			,T.PlanId
 			,T.ModelId
 			
-			FROM Plan_Campaign_Program_Tactic_LineItem L
-			INNER JOIN @Entities T ON T.EntityId = L.PlanTacticId and T.EntityType=''Tactic''
+			FROM Plan_Campaign_Program_Tactic_LineItem L WITH(NOLOCK)
+			INNER JOIN @Entities T ON T.EntityId = L.PlanTacticId and T.EntityType='Tactic'
 			WHERE L.IsDeleted = 0
 	END
 
@@ -1016,17 +1011,14 @@ BEGIN
 	BEGIN
 		Update @Entities SET ColorCode = C.ColorCode, EntityTypeID = T.EntityTypeID
 		FROM @Entities E
-		LEFT JOIN EntityTypeColor C ON C.EntityType = E.EntityType
-		LEFT JOIN EntityType T ON T.Name = E.EntityType
+		LEFT JOIN EntityTypeColor C  WITH(NOLOCK) ON C.EntityType = E.EntityType
+		LEFT JOIN EntityType T WITH(NOLOCK) ON T.Name = E.EntityType
 	END
 END
 
 
 RETURN
 
-END
-
-' 
 END
 
 GO
@@ -1822,37 +1814,35 @@ BEGIN
 	INSERT INTO @tblCustomerFieldIDs
 	SELECT @entCampaign, A.PlanCampaignId, B.CustomFieldId 
 		FROM Plan_Campaign A 
-		JOIN CustomField_Entity B ON A.PlanCampaignId = B.EntityId
-		JOIN CustomField CS ON B.CustomFieldId = CS.CustomFieldId and CS.EntityType=@entCampaign
+		JOIN CustomField_Entity B WITH (NOLOCK) ON A.PlanCampaignId = B.EntityId
+		JOIN CustomField CS WITH (NOLOCK) ON B.CustomFieldId = CS.CustomFieldId and CS.EntityType=@entCampaign
 		WHERE A.IsDeleted='0' and A.PlanId IN (SELECT PlanId FROM @PlanIds)
 	
 	INSERT INTO @tblCustomerFieldIDs
 	SELECT @entProgram, A.PlanProgramId, B.CustomFieldId 
 		FROM Plan_Campaign_Program A 
-		JOIN CustomField_Entity B ON A.PlanProgramId = B.EntityId
-		JOIN CustomField CS ON B.CustomFieldId = CS.CustomFieldId and CS.EntityType=@entProgram
-		JOIN Plan_Campaign C ON A.PlanCampaignId = C.PlanCampaignId and C.IsDeleted='0' and C.PlanId IN (SELECT PlanId FROM @PlanIds)
+		JOIN CustomField_Entity B WITH (NOLOCK) ON A.PlanProgramId = B.EntityId
+		JOIN CustomField CS WITH (NOLOCK) ON B.CustomFieldId = CS.CustomFieldId and CS.EntityType=@entProgram
+		JOIN Plan_Campaign C WITH (NOLOCK) ON A.PlanCampaignId = C.PlanCampaignId and C.IsDeleted='0' and C.PlanId IN (SELECT PlanId FROM @PlanIds)
 		WHERE A.IsDeleted='0'
 	
 	INSERT INTO @tblCustomerFieldIDs
 	SELECT @entTactic, A.PlanTacticId, B.CustomFieldId 
 		FROM Plan_Campaign_Program_Tactic A 
-		JOIN CustomField_Entity B ON A.PlanTacticId = B.EntityId
-		JOIN CustomField CS ON B.CustomFieldId = CS.CustomFieldId and CS.EntityType=@entTactic
-		JOIN Plan_Campaign_Program P ON A.PlanProgramId = P.PlanProgramId and P.IsDeleted='0'
-		JOIN Plan_Campaign C ON P.PlanCampaignId = C.PlanCampaignId and C.IsDeleted='0' and C.PlanId IN (SELECT PlanId FROM @PlanIds)
+		JOIN CustomField_Entity B WITH (NOLOCK) ON A.PlanTacticId = B.EntityId
+		JOIN CustomField CS WITH (NOLOCK) ON B.CustomFieldId = CS.CustomFieldId and CS.EntityType=@entTactic
+		JOIN Plan_Campaign_Program P WITH (NOLOCK)  ON A.PlanProgramId = P.PlanProgramId and P.IsDeleted='0'
+		JOIN Plan_Campaign C WITH (NOLOCK) ON P.PlanCampaignId = C.PlanCampaignId and C.IsDeleted='0' and C.PlanId IN (SELECT PlanId FROM @PlanIds)
 		WHERE A.IsDeleted='0'
 	
 	    SELECT DISTINCT(A.Name) AS [Text],A.EntityType +'Custom'+ Cast(A.CustomFieldId as nvarchar(50)) as Value  
-		FROM CustomField A JOIN CustomFieldType B 
+		FROM CustomField A WITH (NOLOCK) JOIN CustomFieldType B WITH (NOLOCK) 
 		ON A.CustomFieldTypeId = B.CustomFieldTypeId JOIN @tblCustomerFieldIDs C 
 		ON C.CustomFieldID = A.CustomFieldId
 		WHERE A.ClientId=@ClientId AND A.IsDeleted=0 AND A.IsDisplayForFilter=1 AND A.EntityType IN ('Tactic','Campaign','Program') and B.Name='DropDownList' 
 		ORDER BY Value DESC 
 
  END
-
-
 
 GO
 
@@ -8924,8 +8914,8 @@ BEGIN
 				--Hireachy.WorkfrontId AS 'WorkFrontid',
 				--Hireachy.SalesforceId AS 'Salesforceid'
 				FROM [dbo].fnViewByEntityHierarchy(@PlanId,@OwnerIds,@TacticTypeIds,@StatusIds,@ViewBy,@TimeFrame,@Isgrid) Hireachy
-				LEFT JOIN Model M ON Hireachy.ModelId = M.ModelId
-				LEFT JOIN Plan_Campaign_Program_Tactic Tactic ON Hireachy.EntityType='Tactic'
+				LEFT JOIN Model M WITH (NOLOCK) ON Hireachy.ModelId = M.ModelId
+				LEFT JOIN Plan_Campaign_Program_Tactic Tactic WITH (NOLOCK) ON Hireachy.EntityType='Tactic'
 					AND Hireachy.EntityId = Tactic.PlanTacticId
 	
 	OUTER APPLY (SELECT ROI.PlanTacticId
