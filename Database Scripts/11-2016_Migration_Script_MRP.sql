f38d8c7814d2b5ffb35f0eeb1c565656dcf33690
John Zhang <jzhang@hive9.com>
2016-11-22 17:54:43 -0600

    refs #2831 initial write-up for attribution as a SQL trigger

--- Database Scripts/11-2016_Migration_Script_MRP.sql
+++ Database Scripts/11-2016_Migration_Script_MRP.sql
@@ -605,6 +605,98 @@ BEGIN
 END 
 GO
 
+/****** Object:  Trigger [dbo].[UpdateCustomFieldEntityRestrictedTextByUser]    Script Date: 11/22/2016 11:56:34 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+IF EXISTS (SELECT *FROM sys.triggers WHERE OBJECT_ID = OBJECT_ID('[dbo].TransactionCostToLineItemAttribution'))
+	DROP TRIGGER  [dbo].[TransactionCostToLineItemAttribution]
+GO
+
+CREATE TRIGGER [dbo].[TransactionCostToLineItemAttribution] 
+   ON  [dbo].[TransactionLineItemMapping] 
+   AFTER INSERT,UPDATE, DELETE
+AS 
+BEGIN
+	SET NOCOUNT ON;
+
+	--Handle delete (reall delete on this table)
+	DELETE dbo.Plan_Campaign_Program_Tactic_LineItem_Actual
+	FROM DELETED D 
+		JOIN dbo.Plan_Campaign_Program_Tactic_LineItem L ON L.PlanLineItemId = D.LineItemId
+		JOIN dbo.Plan_Campaign_Program_Tactic T ON T.PlanTacticId = L.PlanTacticId 
+		JOIN dbo.Transactions TX ON TX.TransactionId = D.TransactionId
+	WHERE dbo.Plan_Campaign_Program_Tactic_LineItem_Actual.PlanLineItemId = D.LineItemId 
+		AND dbo.Plan_Campaign_Program_Tactic_LineItem_Actual.Period = INT.Period(T.StartDate, TX.AccountingDate)  
+
+    -- We need set the createdBy and CreatedDate for both inserts and updates. 
+	-- NOTE: there is no modified on line item actuals   
+    DECLARE @CreatedBy INT
+	DECLARE @CreatedDate DATETIME
+	SELECT TOP 1  @CreatedBy = INSERTED.ModifiedBy
+			, @CreatedDate = INSERTED.ModifiedBy 
+	FROM INSERTED
+	
+	---Handle updates 
+	UPDATE dbo.Plan_Campaign_Program_Tactic_LineItem_Actual 
+	SET   Value = A.Amount
+		, CreatedBy = @CreatedBy
+		, CreatedDate =@CreatedDate
+	FROM ( 
+			SELECT SUM(M.Amount) AS Amount
+					, L.PlanLineItemId
+					, A.Period
+			FROM	dbo.TransactionLineItemMapping M 
+					JOIN dbo.Plan_Campaign_Program_Tactic_LineItem L ON L.PlanLineItemId = M.LineItemId
+					JOIN dbo.Plan_Campaign_Program_Tactic T ON T.PlanTacticId = L.PlanTacticId
+					JOIN dbo.Transactions TX ON TX.TransactionId = M.TransactionId
+					JOIN dbo.Plan_Campaign_Program_Tactic_LineItem_Actual A ON A.PlanLineItemId = L.PlanLineItemId
+	
+			WHERE A.Period = INT.Period(T.StartDate, TX.AccountingDate) 
+				  AND M.LineItemId IN (SELECT M.LineItemId FROM INSERTED)
+			GROUP BY L.PlanLineItemId, A.Period
+	) A
+	WHERE A.PlanLineItemId = dbo.Plan_Campaign_Program_Tactic_LineItem_Actual.PlanLineItemId
+		AND A.Period = dbo.Plan_Campaign_Program_Tactic_LineItem_Actual.Period
+
+	-- Handle inserts 
+	INSERT INTO  dbo.Plan_Campaign_Program_Tactic_LineItem_Actual(Value, PlanLineItemId, Period, CreatedDate, CreatedBy)
+	SELECT SUM(M.Amount) AS Amount
+			, L.PlanLineItemId
+			, INT.Period(T.StartDate, TX.AccountingDate)
+			, @CreatedDate
+			, @CreatedBy
+	FROM	dbo.TransactionLineItemMapping M 
+			JOIN dbo.Plan_Campaign_Program_Tactic_LineItem L ON L.PlanLineItemId = M.LineItemId
+			JOIN dbo.Plan_Campaign_Program_Tactic T ON T.PlanTacticId = L.PlanTacticId
+			JOIN dbo.Transactions TX ON TX.TransactionId = M.TransactionId
+			LEFT JOIN dbo.Plan_Campaign_Program_Tactic_LineItem_Actual A ON A.PlanLineItemId = L.PlanLineItemId 
+				AND A.Period = INT.Period(T.StartDate, TX.AccountingDate) 	
+	WHERE A.PlanLineItemId IS NULL 
+		AND M.LineItemId IN (SELECT M.LineItemId FROM INSERTED)
+	GROUP BY L.PlanLineItemId, INT.Period(T.StartDate, TX.AccountingDate)
+
+	--Consider the update for 1 to 1 mapped transactions 
+	--NOTE: this step is additive to the above 2 steps 
+	UPDATE dbo.Plan_Campaign_Program_Tactic_LineItem_Actual 
+	SET Value = Value + A.Amount
+	FROM (
+			SELECT SUM(TX.Amount) AS Amount
+					, L.PlanLineItemId
+					, INT.Period(T.StartDate, TX.AccountingDate) AS Period
+			FROM dbo.Transactions TX 
+				JOIN dbo.Plan_Campaign_Program_Tactic_LineItem L ON L.PlanLineItemId = TX.LineItemId
+				JOIN dbo.Plan_Campaign_Program_Tactic T ON T.PlanTacticId = L.PlanTacticId 
+			WHERE TX.LineItemId IN (SELECT Inserted.LineItemId FROM INSERTED) 
+			GROUP BY L.PlanLineItemId, INT.Period(T.StartDate, TX.AccountingDate)
+		) A
+	WHERE dbo.Plan_Campaign_Program_Tactic_LineItem_Actual.PlanLineItemId = A.PlanLineItemId 
+		AND dbo.Plan_Campaign_Program_Tactic_LineItem_Actual.Period = A.Period
+
+END
+
 -- =============================================
 -- Author: Rahul Shah 
 -- Create date: 11/21/2016
