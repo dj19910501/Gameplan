72d6d50d4733f0cf972d715db2c50902e4f1e839
Devanshi <devanshi.gandhi@indusa.com>
2016-11-24 12:38:33 +0530

    changes regarding september patch pl ticket #2834: allow period in input.

--- RevenuePlanner/Scripts/js/scripts.js
+++ RevenuePlanner/Scripts/js/scripts.js
@@ -106,7 +106,9 @@ function GridPriceFormatKeydown(e) {
         // Allow: Ctrl+A
         (e.keyCode == 65 && e.ctrlKey === true) ||
         // Allow: home, end, left, right
-        (e.keyCode >= 35 && e.keyCode <= 39)) {
+        (e.keyCode >= 35 && e.keyCode <= 39) ||
+            //allow period
+         (e.key=="."))   {
         // let it happen, don't do anything
         return;
     }
