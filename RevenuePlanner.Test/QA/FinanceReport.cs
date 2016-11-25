2f2d425970ac3eea7e2ee3e1a43f49dc6483e2b7
Dhvani Raval <dhvani.raval@indusa.com>
2016-11-25 16:33:00 +0530

    Commiting code for 2703

---
+++ RevenuePlanner.Test/QA/FinanceReport.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RevenuePlanner.Test.QA
+{
+    [TestClass]
+    public class FinanceReport
+    {
+        [TestMethod]
+        public void FinanceReportTest()
+        {
+        }
+    }
+}
