eb474945fb9c6330b142ca2db8d994c5ac04f91f
Brandon Wallace <brandon@experoinc.com>
2016-12-01 13:10:18 -0600

    add print statements to vsbuild

--- RevenuePlanner/web-assets/build/vsbuild.cmd
+++ RevenuePlanner/web-assets/build/vsbuild.cmd
@@ -1,11 +1,18 @@
 @ECHO OFF
 
+PATH
+WHERE NODE
+WHERE NPM
+node --version
+
 CD /D "%~dp0\.."
 if %errorlevel% neq 0 exit /b %errorlevel%
 
+node --version
 node build/prebuild.js
 if %errorlevel% neq 0 exit /b %errorlevel%
 
 SET BUILD_CONFIG=%1
+node --version
 npm run cleanbuild
 if %errorlevel% neq 0 exit /b %errorlevel%
