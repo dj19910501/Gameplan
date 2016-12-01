3fc9d5a695709f4628056b1e0fbdb7a88b161e27
Brandon Wallace <brandon@experoinc.com>
2016-12-01 01:42:40 -0600

    Issue #2825 add jquery.simplePagination to transaction grid

--- RevenuePlanner/web-assets/src/transactions/transactionGridDataSource.js
+++ RevenuePlanner/web-assets/src/transactions/transactionGridDataSource.js
@@ -93,7 +93,7 @@ export default function transactionGridDataSource() {
     };
     const paging = {
         skip: 0,
-        take: 3, // TODO make user selectable and/or change default based on Browser speed
+        take: 2, // TODO make user selectable and/or change default based on Browser speed
     };
 
     const dataSource = gridDataSource(filter, paging, undefined, undefined, undefined, getRecordId, getColumnValue);
