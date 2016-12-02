5e607a8ec1fa9b9813311a6bc4997d61f42c093c
Devanshi <devanshi.gandhi@indusa.com>
2016-12-02 14:47:17 +0530

    changes regarding #2804: import marketing budget.
    Add test cases for import file.

--- RevenuePlanner.UnitTest/Service/MarketingBudgetTest.cs
+++ RevenuePlanner.UnitTest/Service/MarketingBudgetTest.cs
@@ -3,6 +3,9 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using StructureMap;
 using RevenuePlanner.Services.MarketingBudget;
 using System.Data;
+using System.IO;
+using System.Reflection;
+using System.Data.OleDb;
 
 namespace RevenuePlanner.UnitTest.Service
 {
@@ -17,7 +20,9 @@ namespace RevenuePlanner.UnitTest.Service
         private const int BudgetId = 80;
         private const int BudgetDetailId = 752;
         private const double ExchangeRate = 1.0; // Currency exchange rate
-
+	    private const string Importviewby="Yearly";//TimeFrame for import budget
+        private const string ImportXLSFileLocation = "\\ImportTest_Data\\grid_MarketingBudget.xls"; // xls file location for import marketing budget
+        private const string ImportXLSX_FileLocation = "\\ImportTest_Data\\grid_MarketingBudgetXLSXFile.xlsx"; // xlsx file location for import marketing budget
         #endregion Test Data
 
         public MarketingBudgetTest()
@@ -53,8 +58,44 @@ namespace RevenuePlanner.UnitTest.Service
             Assert.IsTrue(headerValues.Columns.IndexOf("Planned") > -1);
             Assert.IsTrue(headerValues.Columns.IndexOf("Actual") > -1);
         }
+		    /// <summary>
+        /// Test case for import marketing budget with .XLS file.
+        /// </summary>
+        [TestMethod]
+        public void Test_MarketingBudget_Import()
+        {
+            //read excel file from path for import and return dataset
+            OleDbConnection ExcelConnection; DataSet ds; OleDbDataAdapter Command;
+            var ExcelPath = Directory.GetParent(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)).Parent.FullName + ImportXLSFileLocation;
+            var path = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + ExcelPath + ";Extended Properties=\"Excel 8.0;HDR=YES;\"";
+            ExcelConnection = new OleDbConnection(path);
+            ExcelConnection.Open();
+            string sheetName = "[First_Sheet$]";
+            Command = new OleDbDataAdapter("select * from " + sheetName, ExcelConnection);
+            Command.TableMappings.Add("Table", "TestTable");
+            ds = new DataSet();
+            Command.Fill(ds);
+            ExcelConnection.Close();
+            //end
+            var res = _marketingBudget.GetXLSData(Importviewby, ds,ClientId, BudgetDetailId, 1);
+            Assert.IsTrue(res != null);
+
+        }
+        /// <summary>
+        /// Test case for import marketing budget with .XLSX file.
+        /// </summary>
+        [TestMethod]
+        public void Test_MarketingBudget_Import_XLSXFile()
+        {
+            string FileLocation = Directory.GetParent(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)).Parent.FullName + ImportXLSX_FileLocation;
+            var res = _marketingBudget.GetXLSXData(Importviewby, FileLocation, ClientId, BudgetDetailId, 1);
+            Assert.IsTrue(res!=null);
+
+        }
+      
 
     }
+    
 
     /// <summary>
     /// This extension makes code more readable!
