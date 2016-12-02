b27f86bf4bea823bb58e6877edfd37f59c3ee79c
Arpita Soni <arpita.soni@indusa.com>
2016-12-02 12:23:47 +0530

    Committing changes for test case related to ticket #2788 Get header values from finance.

--- RevenuePlanner.UnitTest/Service/MarketingBudgetTest.cs
+++ RevenuePlanner.UnitTest/Service/MarketingBudgetTest.cs
@@ -2,6 +2,8 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using StructureMap;
 using RevenuePlanner.Services.MarketingBudget;
+using System.Data;
+
 namespace RevenuePlanner.UnitTest.Service
 {
     [TestClass]
@@ -14,6 +16,8 @@ namespace RevenuePlanner.UnitTest.Service
         private const string BudgetTitle = "ZebraAdmin";
         private const int BudgetId = 80;
         private const int BudgetDetailId = 752;
+        private const double ExchangeRate = 1.0; // Currency exchange rate
+
         #endregion Test Data
 
         public MarketingBudgetTest()
@@ -34,10 +38,26 @@ namespace RevenuePlanner.UnitTest.Service
             Assert.IsTrue(res >= 0);
         }
 
+        /// <summary>
+        /// Test case of the method of getting finance header values
+        /// </summary>
+        [TestMethod]
+        public void Test_MarketingBudget_GetFinanceHeaderValues()
+        {
+            // Get header values 
+            DataTable headerValues = _marketingBudget.GetFinanceHeaderValues(BudgetId, ExchangeRate);
+
+            Assert.IsTrue(headerValues.Rows.Count > 0);
+            Assert.IsTrue(headerValues.Columns.IndexOf("Budget") > -1);
+            Assert.IsTrue(headerValues.Columns.IndexOf("Forecast") > -1);
+            Assert.IsTrue(headerValues.Columns.IndexOf("Planned") > -1);
+            Assert.IsTrue(headerValues.Columns.IndexOf("Actual") > -1);
+        }
+
     }
 
     /// <summary>
     /// This extension makes code more readable!
     /// </summary>
-   
+
 }
