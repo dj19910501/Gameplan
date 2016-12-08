ccbd2562376e24ac6f9f0555d14a052137dfb9b7
John Zhang <jzhang@hive9.com>
2016-12-07 18:49:20 -0600

    refs #2825 Added sys_gen_balance 'line item' to show the balance on planned cost

--- Database Scripts/11-2016_Migration_Script_MRP.sql
+++ Database Scripts/11-2016_Migration_Script_MRP.sql
@@ -1122,18 +1122,43 @@ BEGIN
 			, T.Cost AS PlannedCost
 			, SUM(ISNULL(M.Amount, 0.0)) AS TotalLinkedCost --only the portion of the transaction that is linked to this tactic
 			, SUM(ISNULL(LA.Value, 0.0)) AS TotalActual
-
+			, SUM(L.Cost) AS TotalLineItemCost
 	FROM Plan_Campaign_Program_tactic T
 		JOIN dbo.Plan_Campaign_Program_Tactic_LineItem L ON L.PlanTacticId = T.PlanTacticId
 		LEFT JOIN dbo.Plan_Campaign_Program_Tactic_LineItem_Actual LA ON LA.PlanLineItemId = L.PlanLineItemId
 		JOIN dbo.TransactionLineItemMapping M ON M.LineItemId = L.PlanLineItemId
 		JOIN dbo.Transactions TR ON TR.TransactionId = M.TransactionId
+
 	WHERE M.TransactionId = @TransactionId AND TR.ClientID = @ClientId
 	GROUP BY T.PlanTacticId, T.Title, T.Cost
 
 	--dataset 2: line items linked to the @transaction
+
+	DECLARE @LineItemDataTable TABLE (
+					TransactionLineItemMappingId INT NOT NULL,
+					LineItemTotalLinkedCost FLOAT NOT NULL, 
+					LineItemTotalActual FLOAT NOT NULL 
+				)
+
+	INSERT @LineItemDataTable
+	        ( TransactionLineItemMappingId ,
+	          LineItemTotalLinkedCost ,
+	          LineItemTotalActual 
+	        )
+	SELECT   M.TransactionLineItemMappingId
+            , SUM(M.Amount) AS TotalLinkedCost -- SUM is a no-op as a transaction can only be linked once per line item
+			, SUM(ISNULL(LA.Value, 0.0)) AS Actual
+
+	FROM	dbo.TransactionLineItemMapping M 
+			JOIN dbo.Transactions TR ON TR.TransactionId = M.TransactionId
+			JOIN dbo.Plan_Campaign_Program_Tactic_LineItem L ON L.PlanLineItemId = M.LineItemId
+			LEFT JOIN dbo.Plan_Campaign_Program_Tactic_LineItem_Actual LA ON LA.PlanLineItemId = LA.PlanLineItemId
+	WHERE M.TransactionId = @TransactionId AND TR.ClientID = @ClientId
+	GROUP BY M.TransactionLineItemMappingId
+
+
 	SELECT    L.PlanTacticId AS TacticId
-			, L.PlanLineItemId -- this the prmary key, the rest of non aggregate columns are auxiliary info.
+			, L.PlanLineItemId -- this the primary key, the rest of non aggregate columns are auxiliary info.
 			, L.Title
 			, L.Cost AS Cost
             , T.Title AS TacticTitle
@@ -1142,19 +1167,16 @@ BEGIN
 			, PL.Title AS PlanTitle
             , M.TransactionLineItemMappingId
             , M.TransactionId
-            , SUM(M.Amount) AS TotalLinkedCost -- SUM is a no-op as a transaction can only be linked once per line item
-			, SUM(ISNULL(LA.Value, 0.0)) AS Actual
+            , LIT.LineItemTotalLinkedCost AS TotalLinkedCost -- SUM is a no-op as a transaction can only be linked once per line item
+			, LIT.LineItemTotalActual AS Actual
 
 	FROM dbo.Plan_Campaign_Program_Tactic_LineItem L
         JOIN dbo.Plan_Campaign_Program_Tactic T ON T.PlanTacticId = L.PlanTacticId
         JOIN dbo.Plan_Campaign_Program P ON P.PlanProgramId = T.PlanProgramId
         JOIN dbo.Plan_Campaign C ON C.PlanCampaignId = P.PlanCampaignId
         JOIN dbo.[Plan] PL ON PL.PlanId = C.PlanId
-		LEFT JOIN dbo.Plan_Campaign_Program_Tactic_LineItem_Actual LA ON LA.PlanLineItemId = L.PlanLineItemId
 		JOIN dbo.TransactionLineItemMapping M ON M.LineItemId = L.PlanLineItemId
-		JOIN dbo.Transactions TR ON TR.TransactionId = M.TransactionId
-	WHERE M.TransactionId = @TransactionId AND TR.ClientID = @ClientId
-	GROUP BY L.PlanTacticId, L.PlanLineItemId, L.Title, L.Cost, T.Title, P.Title, C.Title, PL.Title, M.TransactionLineItemMappingId, M.TransactionId
+		JOIN @LineItemDataTable LIT ON LIT.TransactionLineItemMappingId = M.TransactionLineItemMappingId
 
 END 
 GO
