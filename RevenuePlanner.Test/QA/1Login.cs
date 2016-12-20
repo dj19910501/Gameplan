5c5d49e7563b71bf6e2f97e60febf5b766bba29e
Dhvani Raval <dhvani.raval@indusa.com>
2016-12-20 19:57:55 +0530

    Commiting Code for ticket #2703

---
+++ RevenuePlanner.Test/QA/1Login.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RevenuePlanner.Controllers;
+using RevenuePlanner.Helpers;
+using RevenuePlanner.Models;
+using System;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using System.Linq;
+using System.Configuration;
+using RevenuePlanner.Test.MockHelpers;
+using RevenuePlanner.Test.QA;
+using System.Data;
+namespace RevenuePlanner.Test.QA
+{
+    [TestClass]
+    public class Login
+    {
+        [TestMethod()]
+        public void A1_Login()
+        {
+            try
+            {
+                Hive9CommonFunctions ObjCommonFunctions = new Hive9CommonFunctions();
+                
+                //Call common function for login
+                var result = ObjCommonFunctions.CheckLogin();
+                Console.WriteLine("LoginController - Index With Parameters");
+                if (result != null)
+                {
+                    Assert.AreEqual("Index", result.RouteValues["Action"]);
+                    Console.WriteLine("\n The assert value of action is " + result.RouteValues["Action"] + ". (The expected value is Index.)");
+
+                    Assert.AreEqual("Home", result.RouteValues["Controller"]);
+                    Console.WriteLine("\n The assert value of controller is " + result.RouteValues["Controller"] + ". (The expected value is Home)");
+
+                    Assert.AreEqual("Home", result.RouteValues["ActiveMenu"]);
+                    Console.WriteLine("\n The assert value of active menu is " + result.RouteValues["ActiveMenu"]+ ". (The expected value is Home)");
+                }
+                else
+                {
+                    Assert.Fail();
+                    Console.WriteLine("\n The assert value is null.");
+                }
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+    }
+}
